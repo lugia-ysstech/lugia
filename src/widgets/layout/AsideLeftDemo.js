0f58816796941b40ad51b88437f0908a9244e788
guoruiguang <guoruiguang@ysstech.com>
2018-12-28 16:28:32 +0800

    新增：     * 组件源信息；

---
+++ src/widgets/layout/AsideLeftDemo.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {Layout} from '@lugia/lugia-web';
+
+const { Header, Content, Footer, Aside } = Layout;
+const header = (
+    <div style={{ height: '60px', lineHeight: '60px', textAlign: 'center', background: '#381be5' }}>
+        Header
+    </div>
+);
+const content = (
+    <div
+        style={{
+            height: '200px',
+            lineHeight: '200px',
+            textAlign: 'center',
+            background: '#0F89FF',
+        }}
+    >
+        Content
+    </div>
+);
+const footer = (
+    <div
+        style={{
+            height: '60px',
+            lineHeight: '60px',
+            textAlign: 'center',
+            background: '#0f13ff',
+        }}
+    >
+        Footer
+    </div>
+);
+const aside = (
+    <div
+        style={{
+            height: '200px',
+            lineHeight: '200px',
+            width: '200px',
+            textAlign: 'center',
+            background: '#11b4ff',
+        }}
+    >
+        Aside
+    </div>
+);
+
+export default class LayoutDemo extends React.Component {
+    render() {
+        return (
+            <Layout direction="row">
+                <Aside collapsible breakpoint={'lg'}>
+                    <div>Aside</div>
+                </Aside>
+                <Layout>
+                    <Header>{header}</Header>
+                    <Content>{content}</Content>
+                    <Footer>{footer}</Footer>
+                </Layout>
+            </Layout>
+        )
+    }
+}
