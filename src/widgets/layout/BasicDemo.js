738e66dda1cd34802ad3b03056f430218865ac3a
grguang <945455382@qq.com>
2018-12-28 16:28:32 +0800

    新增：     * 组件源信息；

---
+++ src/widgets/layout/BasicDemo.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Layout} from '@lugia/lugia-web';
+
+const { Header, Content, Footer, Aside } = Layout;
+const header = (
+    <div style={{ height: '60px', lineHeight: '60px', textAlign: 'center', background: '#381be5' }}>
+        Header
+    </div>
+);
+const content = (
+    <div
+        style={{
+            height: '200px',
+            lineHeight: '200px',
+            textAlign: 'center',
+            background: '#0F89FF',
+        }}
+    >
+        Content
+    </div>
+);
+const footer = (
+    <div
+        style={{
+            height: '60px',
+            lineHeight: '60px',
+            textAlign: 'center',
+            background: '#0f13ff',
+        }}
+    >
+        Footer
+    </div>
+);
+
+export default class LayoutDemo extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <Header>{header}</Header>
+                <Content>{content}</Content>
+                <Footer>{footer}</Footer>
+            </Layout>
+        )
+    }
+}
---
+++ src/widgets/layout/config.js
@@ -0,0 +1,22 @@
+module.exports =  {
+    BasicDemo: {
+        desc: '布局',
+        title: '通用布局'
+    },
+    LeftAsideDemo:{
+        title: '布局',
+        desc: '通用布局'
+    },
+    RightAsideDemo:{
+        title: '布局',
+        desc: '通用布局'
+    },
+    AsideLeftDemo:{
+        title: '布局',
+        desc: '通用布局'
+    },
+    AsideRightDemo:{
+        title: '布局',
+        desc: '通用布局'
+    }
+};
