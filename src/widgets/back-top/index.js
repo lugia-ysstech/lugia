08349d203b08b1e114523cceae4d4d02eab69133
liguoxin <ligx@qq.com>
2018-12-29 14:06:04 +0800

    修改：  * 转义字符问题。

--- src/widgets/back-top/index.js
+++ src/widgets/back-top/index.js
@@ -1,7 +1,7 @@
 import  React from 'react';
         import {Anchor,Grid} from '@lugia/lugia-web';
         import EditTables from '../../edit-table'; 
-        import back-top from '@lugia/lugia-web/dist/back-top/lugia.back-top.zh-CN.json';
+        import backtop from '@lugia/lugia-web/dist/back-top/lugia.back-top.zh-CN.json';
         import Demo from '../code-box';
         import Title from '../code-box/Title';
          const BasicDemo =  require('./BasicDemo').default;  const ThemeDemo =  require('./ThemeDemo').default;  const CustomDemo =  require('./CustomDemo').default;  
@@ -16,7 +16,7 @@ import  React from 'react';
                         <Col span={20}>
                             <Title title={'BackTop 回到顶部'} subTitle={'BackTop'} desc={'返回页面顶部。'} />
                             <Demo title={'基本'} titleID={'back-top-0'} code={<code>{ "import * as React from \'react\';\nimport {BackTop} from \'@lugia/lugia-web\';\n\nexport default class BackTopDemo extends React.Component<any, any> {\n    render() {\n        return (\n            <div>\n                <BackTop />\n                <span>滚动浏览器可查看 BackTop</span>\n            </div>\n        );\n    }\n}\n"}</code>} desc={'最简单的用法'}  demo={<BasicDemo />}></Demo><Demo title={'配置theme主题'} titleID={'back-top-1'} code={<code>{ "import * as React from \'react\';\nimport {BackTop,Theme} from \'@lugia/lugia-web\';\nimport Widget from \'@lugia/lugia-web/dist/consts/index\';\n\nexport default class BackTopDemo extends React.Component {\n\n    render() {\n        const view = {\n            [Widget.BackTop]: {\n                color: \'red\',\n                backgroundColor: \'orange\',\n            },\n        };\n        return (\n            <div>\n                <Theme config={view}>\n                    <BackTop />\n                </Theme>\n                <span>滚动浏览器可查看 BackTop</span>\n            </div>\n        );\n    }\n}\n"}</code>} desc={'根据需要单独配置警告提示样式，可配置 color backgroundColor'}  demo={<ThemeDemo />}></Demo><Demo title={'可关闭'} titleID={'back-top-2'} code={<code>{ "import * as React from \'react\';\nimport {BackTop} from \'@lugia/lugia-web\';\n\nexport default class BackTopDemo extends React.Component<any, any> {\n    render() {\n        return (\n            <div>\n                <BackTop />\n                <span>滚动浏览器可查看 BackTop</span>\n            </div>\n        );\n    }\n}\n"}</code>} desc={'可以关闭的警告提示'}  demo={<CustomDemo />}></Demo>
-                            <EditTables dataSource={back-top} />
+                            <EditTables dataSource={backtop} />
                         </Col>
                         <Col span={4}>
                             <Anchor  slideType="line">
