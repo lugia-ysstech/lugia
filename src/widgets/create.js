067fce259153b2504db0d4241cabcf36858ad42c
liguoxin <ligx@qq.com>
2018-12-29 11:33:57 +0800

    修改：   *  调整生成工具。

--- src/widgets/create.js
+++ src/widgets/create.js
@@ -4,49 +4,67 @@
  *
  */
 const fs = require('fs');
+const path = require('path');
 
-const allPathFile = fs.readdirSync('./');
-const filePath = [];
-allPathFile.forEach(folderName => {
-  fs.stat(folderName, (error, stats) => {
-    if (error) {
-      console.log(error);
-      return;
-    } 
+main();
+
+async function main () {
+  const allPathFile = await fs.readdirSync(__dirname).filter(folderName => folderName !== 'code-box');
+  const filePath = [];
+  console.info('总共待生成组件[%d]个', allPathFile.length);
+  try {
+    for (let index = 0; index < allPathFile.length; index++) {
+      const folderName = allPathFile[ index ];
+      const stats = await fs.statSync(path.join(__dirname, folderName));
       if (stats.isDirectory()) {
-        if (folderName !== 'code-box') {
-          const config = require(`./${folderName}/config.js`);
-          const allDemo = Object.keys(config);
-          // console.log(fs.readFileSync('./affix/index.js', 'utf-8'));
-          const meta = require(`@lugia/lugia-web/dist/${folderName}/lugia.${folderName}.zh-CN.json`);
-          let childrenWidget;
-          if (meta.childrenWidget) {
-            childrenWidget = meta.childrenWidget;
-          }
-          console.log(meta);
-          const data = getContent(allDemo, config, folderName, {
+        let config;
+        try {
+          config = require(`./${folderName}/config.js`);
+        } catch (err) {
+          console.error('(%d) %s 读取config.js错误', index, folderName);
+          continue;
+        }
+
+        const allDemo = Object.keys(config);
+        let meta;
+        try {
+          meta = require(`@lugia/lugia-web/dist/${folderName}/lugia.${folderName}.zh-CN.json`);
+        } catch (error) {
+          console.error('(%d) %s 读取元信息失败', index, folderName);
+          continue;
+        }
+
+        const { childrenWidget } = meta;
+        const subTitle = meta.widgetName;
+        let data;
+        try {
+          data = getContent(allDemo, config, folderName, {
             title: meta.title,
-            subTitle: meta.widgetName,
+            subTitle,
             desc: meta.desc
           }, childrenWidget);
-          console.log(getContent(allDemo, config, folderName, {
-            title: meta.title,
-            subTitle: meta.widgetName,
-            desc: meta.desc
-          }, childrenWidget));
-          try {
-            fs.writeFile(`${folderName}/index.js`, data);
-            console.log('数据已追加到文件');
-          } catch (err) {
-            console.log(err);
-          }
-          filePath.push(folderName);
+        } catch (error) {
+          console.error('(%d) %s 代码生成错误', index, folderName);
+          continue;
         }
+        try {
+          await fs.writeFileSync(path.join(__dirname, `${folderName}/index.js`), data);
+        } catch (err) {
+          console.log('(%d) %s 写入文件失败', index, folderName);
+          return;
+        }
+        filePath.push(folderName);
+      } else {
+        console.error('(%d) %s 目录错误', index, folderName);
       }
-    
 
-  });
-});
+    }
+  } catch (error) {
+    console.error('%s 异常', error);
+  }
+  console.info('总共待生成组件[%d]个， 成功生成[%d]个', allPathFile.length, filePath.length);
+}
+
 
 function getContent (demos, config, folderName, pageInfo, childrenWidget) {
   const { importInfo: ApiImport, demo: ApiTable } = getAPITable(folderName, childrenWidget);
@@ -89,7 +107,7 @@ function getImportInfoAndDemo (demos, config, folderName) {
   let importInfo = '', demo = '', link = '';
   demos.forEach((item, index) => {
     const { title, desc } = config[ item ];
-    const code = fs.readFileSync(`./${folderName}/${item}.js`, 'utf-8');
+    const code = fs.readFileSync(path.join(__dirname, `${folderName}/${item}.js`), 'utf-8');
     const codeStr = code.replace(/\'/g, String.raw`\'`).replace(/\"/g, String.raw`\"`).replace(/\n/g, String.raw`\n`);
     importInfo = `${importInfo} const ${item} =  require('./${item}').default; `;
     demo = `${demo}<Demo title={'${title}'} titleID={'${folderName}-${index}'} code={<code>{ "${codeStr}"}</code>} desc={'${desc}'}  demo={<${item} />}></Demo>`;
