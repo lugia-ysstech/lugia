7fd5b3b79fdcadbdc6f3d47aa5f1333ce18b12e3
SleepyAries <410100498@qq.com>
2019-01-02 17:53:51 +0800

    新增 首页布局

--- src/index.js
+++ src/index.js
@@ -26,5 +26,5 @@ const App = createApp(
 );
 
 render(() => {
-  return <App />;
+  return <Home />;
 }, 'root');
--- src/widgets/index.js
+++ src/widgets/index.js
@@ -3,46 +3,31 @@ import { createBrowserHistory } from 'history';
 import { createRoute } from '@lugia/lugiax-router';
 import '@lugia/lugia-web/dist/css/global.css';
 import Widget from '@lugia/lugia-web/dist/consts/index';
-import {Layout,Theme} from '@lugia/lugia-web';
+import {Grid, Layout, Theme} from '@lugia/lugia-web';
 import Router from '../router';
-
 import Asides from '../menu';
+import styled from 'styled-components';
 
-const { Content,  Aside } = Layout;
+const { Content} = Layout;
+const { Row, Col } = Grid;
+
+
+const InnerContainer = styled.div`
+  width:100%;
+  padding:45px 50px 50px;
+`;
 
 export default () => {
   const rout = createRoute(Router.routerConfig);
-  const layoutView = {
-    [Widget.Layout]: {
-    },
-    [Widget.Content]: {
-      width:900,
-      padding: {
-        top: 20,
-        bottom: 20,
-        left: 20,
-        right: 10,
-      },
-      bordeLeftWidth:1,
-      bordeLeftColor:'#cccccc',
-      bordeLeftStyle:'solid'
-    },
-    [Widget.Aside]: {
-      width:250,
-      backgroundColor:'transparent'
-    },
-  };
-
   return (
-    <div >
-        <Theme config={layoutView}>
-          <Layout direction="row">
-            <Aside><Asides /></Aside>
-            <Content>{rout}</Content>
-          </Layout>
-        </Theme>
-
-    </div>
+      <Row>
+        <Col span={6}  xl={{ span: 4 }}  xxl={{ span: 5 }}>
+          <Asides />
+        </Col>
+        <Col span={18} xl={{ span: 20 }}  xxl={{ span: 19 }}>
+          <InnerContainer>{rout}</InnerContainer>
+        </Col>
+      </Row>
   );
 };
 
