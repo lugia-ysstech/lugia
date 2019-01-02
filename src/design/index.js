0129249dd9b0118be329ec3aeeb4efea35fe03e3
liyaqin <liyaqin@ysstech.com>
2019-01-02 17:53:51 +0800

    新增 首页布局

--- src/design/index.js
+++ src/design/index.js
@@ -3,47 +3,26 @@ import { createBrowserHistory } from 'history';
 import { createRoute } from '@lugia/lugiax-router';
 import '@lugia/lugia-web/dist/css/global.css';
 import Widget from '@lugia/lugia-web/dist/consts/index';
-import {Layout,Theme} from '@lugia/lugia-web';
+import {Grid, Layout, Theme} from '@lugia/lugia-web';
 import Router from '../router';
 
 import Asides from '../menu';
 
 
+const { Row, Col } = Grid;
 const rout = createRoute(Router.designRouter);
 
-const { Content,  Aside } = Layout;
-
 export default () => {
-  const layoutView = {
-    [Widget.Layout]: {
-    },
-    [Widget.Content]: {
-      width:900,
-      padding: {
-        top: 20,
-        bottom: 20,
-        left: 20,
-        right: 10,
-      },
-      bordeLeftWidth:1,
-      bordeLeftColor:'#cccccc',
-      bordeLeftStyle:'solid'
-    },
-    [Widget.Aside]: {
-      width:250,
-      backgroundColor:'transparent'
-    },
-  };
   return (
-    <div >
-      <Theme config={layoutView}>
-        <Layout direction="row">
-          <Aside><Asides /></Aside>
-          <Content>{rout}</Content>
-        </Layout>
-      </Theme>
+    <Row>
+      <Col span={6}  xl={{ span: 4 }}  xxl={{ span: 5 }}>
+        <Asides />
+      </Col>
+      <Col span={18} xl={{ span: 20 }}  xxl={{ span: 19 }}>
+        {rout}
+      </Col>
+    </Row>
 
-    </div>
   );
 };
 
