ca8c0501d0e154c3e14116abdde8625e3711ab39
hadesXx <360998950@qq.com>
2019-01-07 09:31:48 +0800

    提交demo

--- src/widgets/tree/DefaultMutlipleTree.js
+++ src/widgets/tree/DefaultMutlipleTree.js
@@ -1,6 +1,6 @@
 /**
  *
- * create by szfeng
+ * create by ligx
  *
  */
 import * as React from 'react';
@@ -18,7 +18,7 @@ const data = [
     pid: '1.1.1.1',
     path: '1/1.1/1.1.1/1.1.1.1',
     isLeaf: true,
-    notCanSelect: true
+    notCanSelect: true,
   },
   { key: '1.2', title: '1.2', pid: '1', path: '1' },
   { key: '1.2.1', title: '1.2.1', pid: '1.2', path: '1/1.2', isLeaf: true },
@@ -29,7 +29,7 @@ const data = [
     title: '1.2.2.1.1',
     pid: '1.2.2.1',
     path: '1/1.2/1.2.2/1.2.2.1',
-    isLeaf: true
+    isLeaf: true,
   },
   {
     key: '1.2.2.1.2',
@@ -37,40 +37,28 @@ const data = [
     pid: '1.2.2.1',
     path: '1/1.2/1.2.2/1.2.2.1',
     isLeaf: true,
-    notCanSelect: true
+    notCanSelect: true,
   },
-  {
-    key: '1.2.2.2',
-    title: '1.2.2.2',
-    pid: '1.2.2',
-    path: '1/1.2/1.2.2',
-    isLeaf: true
-  },
+  { key: '1.2.2.2', title: '1.2.2.2', pid: '1.2.2', path: '1/1.2/1.2.2', isLeaf: true },
 
   { key: '2', title: '2' },
   { key: '2.1', title: '2.1', pid: '2', path: '2' },
   { key: '2.1.1', title: '2.1.1', pid: '2.1', path: '2/2.1', isLeaf: true },
   { key: '2.1.2', title: '2.1.2', pid: '2.1', path: '2/2.1' },
-  {
-    key: '2.1.2.1',
-    title: '2.1.2.1',
-    pid: '2.1.2',
-    path: '2/2.1/2.1.2',
-    isLeaf: true
-  }
+  { key: '2.1.2.1', title: '2.1.2.1', pid: '2.1.2', path: '2/2.1/2.1.2', isLeaf: true },
 ];
 
-export default class DefaultTree extends React.Component<Object, Object> {
+export default class DefaultMutlipleTree extends React.Component<Object, Object> {
   render() {
     const config = {
       [Widget.Tree]: {
         height: 520,
-        width: 300
-      }
+        width: 300,
+      },
     };
     return (
       <Theme config={config}>
-        <Tree expandAll data={data} igronSelectField={'notCanSelect'} />
+        <Tree expandAll mutliple data={data} igronSelectField={'notCanSelect'} />
       </Theme>
     );
   }
--- src/widgets/tree/config.js
+++ src/widgets/tree/config.js
@@ -3,10 +3,26 @@
  */
 
 module.exports = {
-    DefaultTree: {
-      title: '基本用法',
-      desc: '可以通过closeable变为可关闭标签,动画执行结束后触发onClose事件。默认type为customs风格的标签,主题色为背景色,字体为白色',
-      path: './DefaultTag'
-    },
-  };
-  
+  DefaultTree: {
+    title: '基本用法',
+    desc: '默认为单选,可通过igronSelectField指定禁选的标识符',
+    path: './DefaultTree'
+  },
+  DefaultMutlipleTree: {
+    title: '多选的Tree',
+    desc: '多选的Tree,可通过igronSelectField指定禁选的标识符',
+    path: './DefaultMutlipleTree'
+  },
+  InlineSingleTree: {
+    title: '嵌套数据的单选Tree',
+    desc:
+      '通过指定translateTreeData：true,组件内部将嵌套数据自动转换成Tree型展开数据,可通过disabled指定禁选的项。注意需要指定valueField和displayField',
+    path: './InlineSingleTree'
+  },
+  InlineMutlipleTree: {
+    title: '嵌套数据的多选Tree',
+    desc:
+      '通过指定translateTreeData：true,组件内部将嵌套数据自动转换成Tree型展开数据,可通过disabled指定禁选的项,通过mutliple生成多选Tree。注意需要指定valueField和displayField',
+    path: './InlineMutlipleTree'
+  }
+};
