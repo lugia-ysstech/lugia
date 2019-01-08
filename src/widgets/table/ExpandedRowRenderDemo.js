6bf90f82619768b982ac35f5c0853daf0ade2a7c
grguang <945455382@qq.com>
2019-01-08 09:24:03 +0800

    修改：     增加 table 分页；

---
+++ src/widgets/table/ExpandedRowRenderDemo.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import {Table} from '@lugia/lugia-web';
+
+const columns = [{
+  title: 'Name', dataIndex: 'name', key:'name', width: 100,
+}, {
+  title: 'Age', dataIndex: 'age', key:'age', width: 100,
+}, {
+  title: 'Address', dataIndex: 'address', key:'address', width: 200,
+}, {
+  title: 'Operations', dataIndex: '', key:'operations', render: () => <a href="javascript:;">Delete</a>,
+}];
+
+const data = [
+  { name: 'Uzi', age: 28, address: 'some where', key:'1' ,description: 'I am ADC.' },
+  { name: 'ClearLove', age: 36, address: 'some where', key:'2' ,description: 'I am Jungle.' },
+  { name: 'JackLove', age: 36, address: 'some where', key:'3' ,description: 'I am ADC.' },
+  { name: 'Ming', age: 36, address: 'some where', key:'4' ,description: 'I am Assist .' },
+  { name: 'Rookie', age: 36, address: 'some where', key:'5' ,description: 'I am Ap.' },
+  { name: 'TheShy', age: 36, address: 'some where', key:'6' ,description: 'I am Top.' },
+];
+
+export default class TableDemo extends React.Component {
+  render() {
+    return (
+      <div>
+        <Table
+          columns={columns}
+          expandedRowRender={record => <p style={{ margin: 0 }}>{record.description}</p>}
+          dataSource={data}
+        />
+      </div>
+    );
+  }
+}
