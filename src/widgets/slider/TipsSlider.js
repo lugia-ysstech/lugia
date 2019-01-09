279bc099ab0363732575b6714529067f4a536f2a
cuicuiwang <1623330976@qq.com>
2019-01-09 18:16:11 +0800

    demo文件 包括 date-picker slider switch

--- src/widgets/slider/TipsSlider.js
+++ src/widgets/slider/TipsSlider.js
@@ -2,16 +2,24 @@ import React from 'react';
 import {DemoItem} from './styled'; 
 import { Slider } from '@lugia/lugia-web';
 export default class BaseSlider extends React.Component {
-  onchange = v => {
+  constructor(){
+    super();
+    this.state={
+      tipsValue:'5%'
+    };
+  }
+  onchange = obj => {
+    const {newValue}=obj;
+    this.setState({tipsValue:`${newValue}%`});
   };
   render() {
     return (
       <div>
         <DemoItem>
-          <Slider defaultValue={5} tips/>
+          <Slider defaultValue={5} tips={this.state.tipsValue} onChange={this.onchange}/>
         </DemoItem><br/>
         <DemoItem>
-          <Slider defaultValue={[5,15]} tips/>
+          <Slider defaultValue={5}/>
         </DemoItem>
       </div>
     );
