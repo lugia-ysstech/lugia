9ecea9c416c218cdeb28c3a828963ffb95c9cb2d
zenjava <138962816@qq.com>
2019-01-09 15:39:19 +0800

    修改：   * 文案获取的方式。

---
+++ src/design/page/imgages.js
@@ -0,0 +1,279 @@
+import introduce from '../../../public/design/lugia-logo.png';
+import Alignment from '../../../public/design/alignment.png';
+import Repeat from '../../../public/design/repeat.png';
+import Proximity from '../../../public/design/proximity.png';
+import Hierarchy from '../../../public/design/hierarchy.png';
+import Feedback from '../../../public/design/feedback.png';
+import Guide from '../../../public/design/guide.png';
+import Accessibility from '../../../public/design/accessibility.png';
+import Reduceskip from '../../../public/design/reduceskip.png';
+import coreimg1 from '../../../public/design-core-img1.png';
+import coreimg2 from '../../../public/design-core-img2.png';
+import AlignmentImg from '../../../public/align/align_img.png';
+import alignImg1 from '../../../public/align/align_img1.jpg';
+import alignImg2 from '../../../public/align/align_img2.jpg';
+import alignImg3 from '../../../public/align/align_img3.jpg';
+import alignImg4 from '../../../public/align/align_img4.jpg';
+import alignImg5 from '../../../public/align/align_img5.jpg';
+import alignImg6 from '../../../public/align/align_img6.jpg';
+import RepeatImg from '../../../public/repeat/repeat_img.png';
+import repeatImg1 from '../../../public/repeat/repeat_img1.jpg';
+import repeatImg2 from '../../../public/repeat/repeat_img2.jpg';
+import repeatImg3 from '../../../public/repeat/repeat_img3.jpg';
+import repeatImg4 from '../../../public/repeat/repeat_img4.jpg';
+import proximityImg from '../../../public/proximity/proximity_img.png';
+import proximityImg1 from '../../../public/proximity/proximity_img1.jpg';
+import proximityImg2 from '../../../public/proximity/proximity_img2.jpg';
+import proximityImg3 from '../../../public/proximity/proximity_img3.jpg';
+import proximityImg4 from '../../../public/proximity/proximity_img4.jpg';
+import proximityImg5 from '../../../public/proximity/proximity_img5.jpg';
+import proximityImg6 from '../../../public/proximity/proximity_img6.jpg';
+import hierarchyImg from '../../../public/hierarchy/hierarchy_img.png';
+import hierarchyImg1 from '../../../public/hierarchy/hierarchy_img1.jpg';
+import hierarchyImg2 from '../../../public/hierarchy/hierarchy_img2.jpg';
+import hierarchyImg3 from '../../../public/hierarchy/hierarchy_img3.jpg';
+import feedbackImg from '../../../public/feedback/feedback_img.png';
+import feedbackImg1 from '../../../public/feedback/feedback_img1.jpg';
+import feedbackImg2 from '../../../public/feedback/feedback_img2.jpg';
+import feedbackImg3 from '../../../public/feedback/feedback_img3.jpg';
+import feedbackImg4 from '../../../public/feedback/feedback_img4.jpg';
+import feedbackImg5 from '../../../public/feedback/feedback_img5.jpg';
+import feedbackImg6 from '../../../public/feedback/feedback_img6.jpg';
+import feedbackImg7 from '../../../public/feedback/feedback_img7.jpg';
+import feedbackImg8 from '../../../public/feedback/feedback_img8.jpg';
+import guideImg from '../../../public/guide/guide_img.png';
+import guideImg1 from '../../../public/guide/guide_img1.jpg';
+import guideImg2 from '../../../public/guide/guide_img2.jpg';
+import guideImg3 from '../../../public/guide/guide_img3.jpg';
+import guideImg4 from '../../../public/guide/guide_img4.jpg';
+import guideImg5 from '../../../public/guide/guide_img5.jpg';
+import guideImg6 from '../../../public/guide/guide_img6.jpg';
+import guideImg7 from '../../../public/guide/guide_img7.jpg';
+import accessibilityImg from '../../../public/accessibility/accessibility_img.png';
+import accessibilityImg1 from '../../../public/accessibility/accessibility_img1.jpg';
+import accessibilityImg2 from '../../../public/accessibility/accessibility_img2.jpg';
+import accessibilityImg3 from '../../../public/accessibility/accessibility_img3.jpg';
+import accessibilityImg4 from '../../../public/accessibility/accessibility_img4.jpg';
+import accessibilityImg5 from '../../../public/accessibility/accessibility_img5.jpg';
+import accessibilityImg6 from '../../../public/accessibility/accessibility_img6.jpg';
+import accessibilityImg7 from '../../../public/accessibility/accessibility_img7.jpg';
+import accessibilityImg8 from '../../../public/accessibility/accessibility_img8.jpg';
+import reduceskipImg from '../../../public/reduceskip/reduceskip_img.png';
+import reduceskipImg1 from '../../../public/reduceskip/reduceskip_img1.jpg';
+import reduceskipImg2 from '../../../public/reduceskip/reduceskip_img2.jpg';
+import reduceskipImg3 from '../../../public/reduceskip/reduceskip_img3.jpg';
+import reduceskipImg4 from '../../../public/reduceskip/reduceskip_img4.jpg';
+import reduceskipImg5 from '../../../public/reduceskip/reduceskip_img5.jpg';
+import reduceskipImg6 from '../../../public/reduceskip/reduceskip_img6.jpg';
+import reduceskipImg7 from '../../../public/reduceskip/reduceskip_img7.jpg';
+import reduceskipImg8 from '../../../public/reduceskip/reduceskip_img8.jpg';
+import layoutImg from '../../../public/layout/layout_img.png';
+import layoutImg1 from '../../../public/layout/layout_img1.jpg';
+import layoutImg2 from '../../../public/layout/layout_img2.jpg';
+import layoutImg3 from '../../../public/layout/layout_img3.jpg';
+import layoutImg4 from '../../../public/layout/layout_img4.jpg';
+import layoutImg5 from '../../../public/layout/layout_img5.jpg';
+import patternImg from '../../../public/pattern/pattern_img.png';
+import patternImg1 from '../../../public/pattern/pattern_img1.jpg';
+import patternImg2 from '../../../public/pattern/pattern_img2.jpg';
+import patternImg3 from '../../../public/pattern/pattern_img3.jpg';
+import patternImg4 from '../../../public/pattern/pattern_img4.jpg';
+import patternImg5 from '../../../public/pattern/pattern_img5.jpg';
+import colorImg from '../../../public/color/color_img.png';
+import imgAlice from '../../../public/color/color_img_alice.jpg';
+import imgThemeAlice from '../../../public/color/color_img_theme_alice.jpg';
+import imgDream from '../../../public/color/color_img_dream.jpg';
+import imgThemeDream from '../../../public/color/color_img_theme_dream.jpg';
+import imgBinghan from '../../../public/color/color_img_binghan.jpg';
+import imgThemeBinghan from '../../../public/color/color_img_theme_binghan.jpg';
+import imgBingjing from '../../../public/color/color_img_bingjing.jpg';
+import imgThemeBingjing from '../../../public/color/color_img_theme_bingjing.jpg';
+import imgBinghuo from '../../../public/color/color_img_binghuo.jpg';
+import imgThemeBinghuo from '../../../public/color/color_img_theme_binghuo.jpg';
+import imgGuangxian from '../../../public/color/color_img_guangxian.jpg';
+import imgThemeGuangxian from '../../../public/color/color_img_theme_guangxian.jpg';
+import imgHengxing from '../../../public/color/color_img_hengxing.jpg';
+import imgThemeHengxing from '../../../public/color/color_img_theme_hengxing.jpg';
+import imgHuixing from '../../../public/color/color_img_huixing.jpg';
+import imgThemeHuixing from '../../../public/color/color_img_theme_huixing.jpg';
+import imgHuorong from '../../../public/color/color_img_huorong.jpg';
+import imgThemeHuorong from '../../../public/color/color_img_theme_huorong.jpg';
+import imgJiguang from '../../../public/color/color_img_jiguang.jpg';
+import imgThemeJiguang from '../../../public/color/color_img_theme_jiguang.jpg';
+import imgKejilan from '../../../public/color/color_img_kejilan.jpg';
+import imgThemeKejilan from '../../../public/color/color_img_theme_kejilan.jpg';
+import imgScience from '../../../public/color/color_img_science.jpg';
+import imgThemeScience from '../../../public/color/color_img_theme_science.jpg';
+import imgRongxue from '../../../public/color/color_img_rongxue.jpg';
+import imgThemeRongxue from '../../../public/color/color_img_theme_rongxue.jpg';
+import imgTaixian from '../../../public/color/color_img_taixian.jpg';
+import imgThemeTaixian from '../../../public/color/color_img_theme_taixian.jpg';
+import imgWarm from '../../../public/color/color_img_warm.jpg';
+import imgThemeWarm from '../../../public/color/color_img_theme_warm.jpg';
+import imgLucky from '../../../public/color/color_img_lucky.jpg';
+import imgThemeLucky from '../../../public/color/color_img_theme_lucky.jpg';
+import imgYinyun from '../../../public/color/color_img_yinyun.jpg';
+import imgThemeYinyun from '../../../public/color/color_img_theme_yinyun.jpg';
+import imgManor from '../../../public/color/color_img_manor.jpg';
+import imgThemeManor from '../../../public/color/color_img_theme_manor.jpg';
+import imgViolet from '../../../public/color/color_img_violet.jpg';
+import imgThemeViolet from '../../../public/color/color_img_theme_violet.jpg';
+import imgNature from '../../../public/color/color_img_nature.jpg';
+import imgThemeNature from '../../../public/color/color_img_theme_nature.jpg';
+import fontImg from '../../../public/font/font_img.png';
+import PingFangSC from '../../../public/font/PingFangSC.png';
+import hiaginosans from '../../../public/font/hiaginosans.png';
+import yahei from '../../../public/font/yahei.png';
+import helveticaneue from '../../../public/font/helveticaneue.png';
+import helvetica from '../../../public/font/helvetica.png';
+import arial from '../../../public/font/arial.png';
+import fontImg1 from '../../../public/font/font_img1.jpg';
+import fontImg2 from '../../../public/font/font_img2.jpg';
+import fontImg3 from '../../../public/font/font_img3.jpg';
+import iconImg from '../../../public/icon/icon_img.png';
+import iconImg1 from '../../../public/icon/icon_img1.jpg';
+import iconImg2 from '../../../public/icon/icon_img2.jpg';
+import iconImg3 from '../../../public/icon/icon_img3.jpg';
+import iconImg4 from '../../../public/icon/icon_img4.jpg';
+import iconImg5 from '../../../public/icon/icon_img5.jpg';
+
+export default {
+  introduce,
+  Alignment,
+  Repeat,
+  Proximity,
+  Hierarchy,
+  Feedback,
+  Guide,
+  Accessibility,
+  Reduceskip,
+  coreimg1,
+  coreimg2,
+  AlignmentImg,
+  alignImg1,
+  alignImg2,
+  alignImg3,
+  alignImg4,
+  alignImg5,
+  alignImg6,
+  RepeatImg,
+  repeatImg1,
+  repeatImg2,
+  repeatImg3,
+  repeatImg4,
+  proximityImg,
+  proximityImg1,
+  proximityImg2,
+  proximityImg3,
+  proximityImg4,
+  proximityImg5,
+  proximityImg6,
+  hierarchyImg,
+  hierarchyImg1,
+  hierarchyImg2,
+  hierarchyImg3,
+  feedbackImg,
+  feedbackImg1,
+  feedbackImg2,
+  feedbackImg3,
+  feedbackImg4,
+  feedbackImg5,
+  feedbackImg6,
+  feedbackImg7,
+  feedbackImg8,
+  guideImg,
+  guideImg1,
+  guideImg2,
+  guideImg3,
+  guideImg4,
+  guideImg5,
+  guideImg6,
+  guideImg7,
+  accessibilityImg,
+  accessibilityImg1,
+  accessibilityImg2,
+  accessibilityImg3,
+  accessibilityImg4,
+  accessibilityImg5,
+  accessibilityImg6,
+  accessibilityImg7,
+  accessibilityImg8,
+  reduceskipImg,
+  reduceskipImg1,
+  reduceskipImg2,
+  reduceskipImg3,
+  reduceskipImg4,
+  reduceskipImg5,
+  reduceskipImg6,
+  reduceskipImg7,
+  reduceskipImg8,
+  layoutImg,
+  layoutImg1,
+  layoutImg2,
+  layoutImg3,
+  layoutImg4,
+  layoutImg5,
+  patternImg,
+  patternImg1,
+  patternImg2,
+  patternImg3,
+  patternImg4,
+  patternImg5,
+  colorImg,
+  imgAlice,
+  imgThemeAlice,
+  imgDream,
+  imgThemeDream,
+  imgBinghan,
+  imgThemeBinghan,
+  imgBingjing,
+  imgThemeBingjing,
+  imgBinghuo,
+  imgThemeBinghuo,
+  imgGuangxian,
+  imgThemeGuangxian,
+  imgHengxing,
+  imgThemeHengxing,
+  imgHuixing,
+  imgThemeHuixing,
+  imgHuorong,
+  imgThemeHuorong,
+  imgJiguang,
+  imgThemeJiguang,
+  imgKejilan,
+  imgThemeKejilan,
+  imgScience,
+  imgThemeScience,
+  imgRongxue,
+  imgThemeRongxue,
+  imgTaixian,
+  imgThemeTaixian,
+  imgWarm,
+  imgThemeWarm,
+  imgLucky,
+  imgThemeLucky,
+  imgYinyun,
+  imgThemeYinyun,
+  imgManor,
+  imgThemeManor,
+  imgViolet,
+  imgThemeViolet,
+  imgNature,
+  imgThemeNature,
+  fontImg,
+  PingFangSC,
+  hiaginosans,
+  yahei,
+  helveticaneue,
+  helvetica,
+  arial,
+  fontImg1,
+  fontImg2,
+  fontImg3,
+  iconImg,
+  iconImg1,
+  iconImg2,
+  iconImg3,
+  iconImg4,
+  iconImg5
+};
