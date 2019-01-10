4af128982718d85414e5e1d2d539d8336ec6ce6e
grguang <945455382@qq.com>
2019-01-10 16:22:40 +0800

    修改：     button 增加间距；

--- src/widgets/button/CircleDemo.js
+++ src/widgets/button/CircleDemo.js
@@ -1,12 +1,18 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import {Button} from '@lugia/lugia-web';
+
+const Empty = styled.span`
+  display: inline-block;
+  width: 10px;
+`;
 
 export default class ButtonDemo extends React.Component<any, any> {
     render() {
         return (
             <div>
-                <Button circle icon="lugia-icon-direction_logout" />
-                <Button circle icon="lugia-icon-financial_search" />
+                <Button circle icon="lugia-icon-direction_logout" /><Empty />
+                <Button type="primary" circle icon="lugia-icon-financial_search" /><Empty />
                 <Button circle icon="lugia-icon-financial_download" />
             </div>
         );
