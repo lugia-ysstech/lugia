8a2034a3bedd1ade0c98bd0a79e008d8366d748f
liguoxin <ligx@qq.com>
2019-01-10 11:19:13 +0800

    修改：   * 增加查询代理接口。

--- mock.config.js
+++ mock.config.js
@@ -1,18 +1,23 @@
-import random from "./mock/random";
-
+require('isomorphic-fetch');
 export default {
-  ...random,
-  "/api/array": [
-    { name: "apple", value: 1.2 },
-    { name: "orange", value: 0.95 }
+  'POST /api/array': [
+    { name: 'apple', value: 1.2 },
+    { name: 'orange', value: 0.95 }
   ],
-  "/api/object": {
+  '/api/object': {
     shop: {
       taxPercent: 8,
-      items: [{ name: "apple", value: 1.2 }, { name: "orange", value: 0.95 }]
+      items: [{ name: 'apple', value: 1.2 }, { name: 'orange', value: 0.95 }]
     }
   },
-  "/api/function": function(req, res) {
-    res.end("Mock Function.");
+  'POST /api/search': function (req, res) {
+    fetch('http://192.168.102.79/api/search',
+      {
+        method: 'Post',
+        body: JSON.stringify(req.body ),
+        headers: new Headers({ 'Content-Type': 'application/json' }),
+      }).then(response => (response.json())).then(data => {
+        res.json(data);
+    });
   }
 };
