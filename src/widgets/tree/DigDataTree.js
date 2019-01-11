2882b1a44035dec26920d2b41cd14f1858259d1e
shizhenfeng <shizhenfeng@ysstech.com>
2019-01-11 17:13:49 +0800

    修改：门户Demo

--- src/widgets/tree/DigDataTree.js
+++ src/widgets/tree/DigDataTree.js
@@ -4,7 +4,7 @@
  *
  */
 import * as React from 'react';
-import { Tree, Theme } from '@lugia/lugia-web';
+import { Tree, Theme, Input } from '@lugia/lugia-web';
 import Widget from '@lugia/lugia-web/dist/consts';
 
 const bigTree = [];
@@ -56,20 +56,39 @@ function getNumberKey() {
 export default class BigDataTree extends React.Component {
   constructor(props) {
     super(props);
-    const { value } = props;
-    this.state = { value };
+    this.state = { query: '' };
   }
 
   render() {
-    const config = {
+    const { query } = this.state;
+    const treeConfig = {
       [Widget.Tree]: {
-        height: 500
+        height: 500,
+        width: 220
+      }
+    };
+
+    const inputConfig = {
+      [Widget.Tree]: {
+        width: 220
       }
     };
     return [
-      <Theme config={config}>
-        <Tree data={bigTree} expandAll={true} mutliple />
+      <Theme config={inputConfig}>
+        <Input
+          value={query}
+          placeholder={'请输入查询条件'}
+          onChange={this.onChange}
+        />
+      </Theme>,
+      <Theme config={treeConfig}>
+        <Tree query={query} data={bigTree} expandAll mutliple />
       </Theme>
     ];
   }
+
+  onChange = target => {
+    const { newValue: query } = target;
+    this.setState({ query });
+  };
 }
