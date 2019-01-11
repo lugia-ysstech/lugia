adf04b8e5758bd932e3b985e03e159f75967d131
grguang <945455382@qq.com>
2019-01-11 10:37:21 +0800

    修改：     alert demo 空行；     anchor demo 展示问题；

--- src/widgets/anchor/TypeDemo.js
+++ src/widgets/anchor/TypeDemo.js
@@ -6,15 +6,11 @@ const { Link } = Anchor;
 export default class AnchorDemo extends React.Component {
     render(){
         return (
-            <div>
+            <div style={{marginLeft: '400px'}}>
                 <Anchor slideType="none">
-                    <Link title="anchor-link1" href="#anchor-link1" />
-                    <Link title="anchor-link2" href="#anchor-link2" />
-                    <Link title="anchor-link3" href="#anchor-link3">
-                        <Link title="anchor-link3.1" href="#anchor-link3.1" />
-                        <Link title="anchor-link3.2" href="#anchor-link3.2" />
-                    </Link>
-                    <Link title="anchor-link4" href="#anchor-link4" />
+                  <Link title="基本用法" href="#anchor-0" />
+                  <Link title="指定容器" href="#anchor-1" />
+                  <Link title="固定状态改变的回调" href="#anchor-2" />
                 </Anchor>
             </div>
 
