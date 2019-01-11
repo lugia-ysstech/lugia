a1ba143d86fee4b75a49bda30b776492db0af13a
zenjava <138962816@qq.com>
2019-01-11 19:09:23 +0800

    修改：   * 重新生成demo。

--- src/router/widgets.js
+++ src/router/widgets.js
@@ -1,8 +1,8 @@
-export default [{
+export default [ {
   text: '数据录入',
   category: '数据录入',
   describe: true,
-  children: [{
+  children: [ {
     widgetName: 'Affix',
     floderName: 'affix',
     value: '/component/affix',
@@ -62,7 +62,7 @@ export default [{
     floderName: 'dropmenu',
     value: '/component/dropmenu',
     text: 'Dropmenu 下拉菜单'
-  }, {widgetName: 'Input', floderName: 'input', value: '/component/input', text: 'Input 文本输入框'}, {
+  }, { widgetName: 'Input', floderName: 'input', value: '/component/input', text: 'Input 文本输入框' }, {
     widgetName: 'Menu',
     floderName: 'menu',
     value: '/component/menu',
@@ -97,7 +97,7 @@ export default [{
     floderName: 'progress',
     value: '/component/progress',
     text: 'Progress 进度条'
-  }, {widgetName: 'Radio', floderName: 'radio', value: '/component/radio', text: 'Radio 单选框'}, {
+  }, { widgetName: 'Radio', floderName: 'radio', value: '/component/radio', text: 'Radio 单选框' }, {
     widgetName: 'Rate',
     floderName: 'rate',
     value: '/component/rate',
@@ -112,7 +112,12 @@ export default [{
     floderName: 'slider',
     value: '/component/slider',
     text: 'Slider 滑动输入条'
-  }, {widgetName: 'Switch', floderName: 'switch', value: '/component/switch', text: 'Switch 开关'}, {
+  }, {
+    widgetName: 'Switch',
+    floderName: 'switch',
+    value: '/component/switch',
+    text: 'Switch 开关'
+  }, {
     widgetName: 'Table',
     floderName: 'table',
     value: '/component/table',
@@ -127,22 +132,27 @@ export default [{
     floderName: 'transfer',
     value: '/component/transfer',
     text: 'Transfer 穿梭框'
-  }, {widgetName: 'Tree', floderName: 'tree', value: '/component/tree', text: 'Tree 树形控件'}, {
+  }, {
+    widgetName: 'Tree',
+    floderName: 'tree',
+    value: '/component/tree',
+    text: 'Tree 树形控件'
+  }, {
     widgetName: 'TreeSelect',
     floderName: 'tree-select',
     value: '/component/treeselect',
     text: 'TreeSelect 树形选择控件'
-  }, {widgetName: 'Upload', floderName: 'upload', value: '/component/upload', text: 'Upload 上传'}]
+  }, { widgetName: 'Upload', floderName: 'upload', value: '/component/upload', text: 'Upload 上传' } ]
 }, {
   text: '数据展示',
   category: '数据展示',
   describe: true,
-  children: [{
+  children: [ {
     widgetName: 'Avatar',
     floderName: 'avatar',
     value: '/component/avatar',
     text: 'Avatar 头像'
-  }, {widgetName: 'Badge', floderName: 'badge', value: '/component/badge', text: 'Badge 徽标数'}, {
+  }, { widgetName: 'Badge', floderName: 'badge', value: '/component/badge', text: 'Badge 徽标数' }, {
     widgetName: 'Card',
     floderName: 'card',
     value: '/component/card',
@@ -157,7 +167,7 @@ export default [{
     floderName: 'popover',
     value: '/component/popover',
     text: 'Popover 气泡卡片'
-  }, {widgetName: 'Tabs', floderName: 'tabs', value: '/component/tabs', text: 'Tabs 标签页'}, {
+  }, { widgetName: 'Tabs', floderName: 'tabs', value: '/component/tabs', text: 'Tabs 标签页' }, {
     widgetName: 'Tag',
     floderName: 'tag',
     value: '/component/tag',
@@ -167,60 +177,60 @@ export default [{
     floderName: 'time-line',
     value: '/component/timeline',
     text: 'Timeline 时间轴'
-  }, {widgetName: 'Tooltip', floderName: 'tooltip', value: '/component/tooltip', text: 'Tooltip 文字提示'}]
+  }, { widgetName: 'Tooltip', floderName: 'tooltip', value: '/component/tooltip', text: 'Tooltip 文字提示' } ]
 }, {
   text: '通用',
   category: '通用',
   describe: true,
-  children: [{
+  children: [ {
     widgetName: 'Breadcrumb',
     floderName: 'breadcrumb',
     value: '/component/breadcrumb',
     text: 'Breadcrumb 面包屑'
-  }, {widgetName: 'Button', floderName: 'button', value: '/component/button', text: 'Button 按钮'}, {
-    widgetName: 'Icon',
-    floderName: 'icon',
-    value: '/component/icon',
-    text: 'Icon 图标'
-  }]
+  }, {
+    widgetName: 'Button',
+    floderName: 'button',
+    value: '/component/button',
+    text: 'Button 按钮'
+  }, { widgetName: 'Icon', floderName: 'icon', value: '/component/icon', text: 'Icon 图标' } ]
 }, {
   text: '其他',
   category: '其他',
   describe: true,
-  children: [{
+  children: [ {
     widgetName: 'Divider',
     floderName: 'divider',
     value: '/component/divider',
     text: 'Divider 分割线'
-  }, {widgetName: 'Loading', floderName: 'loading', value: '/component/loading', text: 'Loading 加载中'}]
+  }, { widgetName: 'Loading', floderName: 'loading', value: '/component/loading', text: 'Loading 加载中' } ]
 }, {
   text: '布局',
   category: '布局',
   describe: true,
-  children: [{widgetName: 'Grid', floderName: 'grid', value: '/component/grid', text: 'Grid 栅格'}, {
-    widgetName: 'Layout',
-    floderName: 'layout',
-    value: '/component/layout',
-    text: 'Layout 布局'
-  }]
+  children: [ {
+    widgetName: 'Grid',
+    floderName: 'grid',
+    value: '/component/grid',
+    text: 'Grid 栅格'
+  }, { widgetName: 'Layout', floderName: 'layout', value: '/component/layout', text: 'Layout 布局' } ]
 }, {
   text: '导航',
   category: '导航',
   describe: true,
-  children: [{
+  children: [ {
     widgetName: 'NavMenu',
     floderName: 'navmenu',
     value: '/component/navmenu',
     text: 'NavMenu 导航菜单'
-  }, {widgetName: 'Steps', floderName: 'steps', value: '/component/steps', text: 'Steps 步骤条'}]
+  }, { widgetName: 'Steps', floderName: 'steps', value: '/component/steps', text: 'Steps 步骤条' } ]
 }, {
   text: '反馈',
   category: '反馈',
   describe: true,
-  children: [{
+  children: [ {
     widgetName: 'Popconfirm',
     floderName: 'popconfirm',
     value: '/component/popconfirm',
     text: 'Popconfirm 气泡确认框'
-  }, {widgetName: 'Skeleton', floderName: 'skeleton', value: '/component/skeleton', text: 'Skeleton 加载占位符'}]
-}];
+  }, { widgetName: 'Skeleton', floderName: 'skeleton', value: '/component/skeleton', text: 'Skeleton 加载占位符' } ]
+} ];
