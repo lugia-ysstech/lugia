72ae8a14848fb55884653d2d20acf13ffc99c827
jelly@pudding <liang_guodong@126.com>
2019-01-25 11:35:12 +0800

    修改     *更新demo

--- src/widgets/icon/BaseIcon.js
+++ src/widgets/icon/BaseIcon.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { Icon, Theme } from '@lugia/lugia-web';
+import { Icon, notification, Theme } from '@lugia/lugia-web';
 import Widget from '@lugia/lugia-web/dist/consts';
-const onClick = () => {};
+import styled from 'styled-components';
+
+const ColorIcon = styled(Icon)`
+  color: #4d63ff;
+`;
+document.addEventListener('copy', e => {
+  const cd = e.clipboardData;
+  cd.setData('text/plain', window.__iconClass__);
+  delete window.__iconClass__;
+  e.preventDefault();
+  return false;
+});
+
+const onHandleClick = iconClass => {
+  window.__iconClass__ = iconClass;
+  document.execCommand('copy');
+  notification.open({
+    icon: 'lugia-icon-reminder_check_circle',
+    title: `已复制 ${iconClass}`
+  });
+};
 export default class BaseIcon extends React.Component<any, any> {
   render() {
     const view = {
@@ -12,17 +32,30 @@ export default class BaseIcon extends React.Component<any, any> {
           top: 10,
           bottom: 0
         },
-        fontSize:20
+        fontSize: 20
       }
     };
+
     return (
       <Theme config={view}>
-        <Icon iconClass={'lugia-icon-financial_unlock'} onClick={onClick} />
-        <Icon
-          iconClass={'lugia-icon-financial_upload_cloud'}
-          onClick={onClick}
+        <ColorIcon
+          iconClass={'lugia-icon-financial_unlock'}
+          onClick={() => {
+            onHandleClick('lugia-icon-financial_unlock');
+          }}
         />
-        <Icon iconClass={'lugia-icon-financial_pay'} onClick={onClick} />
+        <ColorIcon
+          iconClass={'lugia-icon-financial_upload_cloud'}
+          onClick={() => {
+            onHandleClick('lugia-icon-financial_upload_cloud');
+          }}
+        />
+        <ColorIcon
+          iconClass={'lugia-icon-financial_pay'}
+          onClick={() => {
+            onHandleClick('lugia-icon-financial_pay');
+          }}
+        />
       </Theme>
     );
   }
