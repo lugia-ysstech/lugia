95c4b6c3d379d6d107b2a5911a34674366670965
cuicuiwang <1623330976@qq.com>
2019-07-18 20:19:02 +0800

    demo文件 upload 测试

---
+++ src/widgets/upload/test/Disabled.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Upload, Theme } from '@lugia/lugia-web';
+export default class Disabled extends React.Component<any, any> {
+  render(){
+    return (
+      <Upload disabled/>
+    )
+  }
+}
