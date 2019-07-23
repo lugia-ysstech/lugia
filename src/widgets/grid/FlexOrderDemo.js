42cdd5c3ccc6ca096a31b2d55fb0a71855c7aa2a
grguang <945455382@qq.com>
2019-07-23 13:14:41 +0800

    新增：     * grid 增加背景色；

--- src/widgets/grid/FlexOrderDemo.js
+++ src/widgets/grid/FlexOrderDemo.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import {Grid} from '@lugia/lugia-web';
+import styled from 'styled-components';
+
 const {Row, Col} = Grid;
+const Common = styled.div`
+  text-align: center;
+  line-height: 50px;
+`;
+const OddItem = styled(Common)`
+  background-color: rgba(77, 99, 255, 0.5);
+`;
+const EvenItem = styled(Common)`
+  background-color: rgba(128, 129, 255);
+`;
 
 export default class GridDemo extends React.Component {
     render() {
@@ -8,16 +20,16 @@ export default class GridDemo extends React.Component {
             <div>
                 <Row type="flex" justify="center">
                     <Col span={4} order={3}>
-                        col-4 center-order-3
+                      <OddItem>col-4 order-3</OddItem>
                     </Col>
                     <Col span={4} order={2}>
-                        col-4 center-order-2
+                      <EvenItem>col-4 order-2</EvenItem>
                     </Col>
                     <Col span={4} order={1}>
-                        col-4 center-order-1
+                      <OddItem>col-4 order-1</OddItem>
                     </Col>
                     <Col span={4} order={4}>
-                        col-4 center-order-4
+                      <EvenItem>col-4 order-4</EvenItem>
                     </Col>
                 </Row>
             </div>
