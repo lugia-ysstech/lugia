19cd4e2a1a715d8b4c0514bd2ece26acaf2b8239
guoruiguang <guoruiguang@ysstech.com>
2019-07-23 13:14:41 +0800

    新增：     * grid 增加背景色；

--- src/widgets/grid/BasicDemo.js
+++ src/widgets/grid/BasicDemo.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 import {Grid} from '@lugia/lugia-web';
 const {Row, Col} = Grid;
+
+const Common = styled.div`
+  text-align: center;
+  line-height: 50px;
+`;
+const OddItem = styled(Common)`
+  background-color: rgba(77, 99, 255, 0.5);
+`;
+const EvenItem = styled(Common)`
+  background-color: rgba(128, 129, 255);
+`;
 
 export default class GridDemo extends React.Component {
     render() {
         return (
             <div>
                 <Row>
-                    <Col span={6}>col-6</Col>
-                    <Col span={6}>col-6</Col>
-                    <Col span={6}>col-6</Col>
-                    <Col span={6}>col-6</Col>
+                    <Col span={6}><OddItem>col-6</OddItem></Col>
+                    <Col span={6}><EvenItem>col-6</EvenItem></Col>
+                    <Col span={6}><OddItem>col-6</OddItem></Col>
+                    <Col span={6}><EvenItem>col-6</EvenItem></Col>
                 </Row>
             </div>
         );
--- src/widgets/grid/OffsetDemo.js
+++ src/widgets/grid/OffsetDemo.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import {Grid} from '@lugia/lugia-web';
+import styled from 'styled-components';
+
 const {Row, Col} = Grid;
+const Common = styled.div`
+  text-align: center;
+  line-height: 50px;
+`;
+const OddItem = styled(Common)`
+  background-color: rgba(77, 99, 255, 0.5);
+`;
+const EvenItem = styled(Common)`
+  background-color: rgba(128, 129, 255);
+`;
 
 export default class GridDemo extends React.Component {
     render() {
@@ -8,10 +20,10 @@ export default class GridDemo extends React.Component {
             <div>
                 <Row>
                     <Col span={6} offset={6}>
-                        col-6 col-offset-6
+                      <OddItem>col-6 col-offset-6</OddItem>
                     </Col>
                     <Col span={6} offset={6}>
-                        col-6 col-offset-6
+                      <EvenItem>col-6 col-offset-6</EvenItem>
                     </Col>
                 </Row>
             </div>
