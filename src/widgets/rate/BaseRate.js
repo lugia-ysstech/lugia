78ca868be4af306badf03b541bcbe12bbc4c0947
SleepyAries <410100498@qq.com>
2019-08-05 16:38:42 +0800

    修改：     * lugia-web版本更新

--- src/widgets/rate/BaseRate.js
+++ src/widgets/rate/BaseRate.js
@@ -11,57 +11,55 @@ export default class RateDemo extends React.Component<any, any> {
   constructor(props: Object) {
     super(props);
     this.state = {
-      defaultProps:3,
-      defaultProps7:0
+      defaultPropsConfig : {
+        count: 10,
+        max: 10,
+        value: 3,
+        disabled: false,
+        allowHalf: false,
+        classify: false,
+        onClick: (e: Object, x: any) => {
+          this.setStateValue('defaultPropsConfig', 'value',x.newValue);
+        },
+        onChange: (e: Object, x: any) => {}
+      },
+      defaultProps7Config : {
+        count: 5,
+        value: 0,
+        allowHalf: true,
+        onClick: (e: Object, x: any) => {
+          this.setStateValue('defaultProps7Config','value', x.newValue);
+        },
+        onChange: (e: Object, x: any) => {
+          // this.setStateValue('defaultProps7Config','value', x.newValue);
+        }
+      }
     };
   }
   render() {
     const config = {
       [Widget.Rate]: { fontSize: '18px' }
     };
-    const defaultPropsConfig = {
-      count: 10,
-      max: 10,
-      disabled: false,
-      allowHalf: false,
-      classify: false,
-      onClick: (e: Object, x: any) => {
-        this.setStateValue('defaultProps', x.newValue);
-      },
-      onChange: (e: Object, x: any) => {
-        this.setStateValue('defaultProps', x.newValue);
-      }
-    };
-    const defaultProps7Config = {
-      count: 5,
-      allowHalf: true,
-      onClick: (e: Object, x: any) => {
-        this.setStateValue('defaultProps7', x.newValue);
-      },
-      onChange: (e: Object, x: any) => {
-        this.setStateValue('defaultProps7', x.newValue);
-      }
-    };
-    const {defaultProps,defaultProps7} = this.state;
     return (
       <div>
         <div>
           <div>
             <Rate />
           </div>
-          <Rate {...defaultProps7Config} value={defaultProps7} />
-          <TextBox>{defaultProps7} 颗星</TextBox>
+          <Rate {...this.state.defaultProps7Config}  />
+          <TextBox>{this.state.defaultProps7Config.value} 颗星</TextBox>
         </div>
         <Theme config={config}>
-          <Rate {...defaultPropsConfig} value={defaultProps} />
-          <TextBox>{defaultProps} 颗星</TextBox>
+          <Rate {...this.state.defaultPropsConfig}  />
+          <TextBox>{this.state.defaultPropsConfig.value} 颗星</TextBox>
         </Theme>
       </div>
     );
   }
-  setStateValue = (target: string, val: number) => {
+  setStateValue = (target: string, props: string, val: number) => {
+    const data = Object.assign(this.state[target], { value: val });
     this.setState({
-      [target]: val
+      [target]: data,
     });
   };
 }
