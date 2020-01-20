7e851963a58d353716aa764cf5fb7e7fbc7a5293
jelly@pudding <liang_guodong@126.com>
2020-01-20 18:55:51 +0800

    Update    update layout demo

--- src/widgets/layout/AsideLeftDemo.js
+++ src/widgets/layout/AsideLeftDemo.js
@@ -1,53 +1,57 @@
-import React from 'react';
-import {Layout,Theme} from '@lugia/lugia-web';
-import Widget from '@lugia/lugia-web/dist/consts/index';
+import React from "react";
+import { Layout } from "@lugia/lugia-web";
 
 const { Header, Content, Footer, Aside } = Layout;
 const header = (
-    <div style={{ height: '60px', lineHeight: '60px', textAlign: 'center', background: '#F0F2FF' }}>
-        Header
-    </div>
+  <div
+    style={{
+      height: "60px",
+      lineHeight: "60px",
+      textAlign: "center",
+      background: "#F0F2FF"
+    }}
+  >
+    Header
+  </div>
 );
 const content = (
-    <div
-        style={{
-            height: '200px',
-            lineHeight: '200px',
-            textAlign: 'center',
-            background: '#B7C0FF',
-        }}
-    >
-        Content
-    </div>
+  <div
+    style={{
+      height: "200px",
+      lineHeight: "200px",
+      textAlign: "center",
+      background: "#B7C0FF"
+    }}
+  >
+    Content
+  </div>
 );
 const footer = (
-    <div
-        style={{
-            height: '60px',
-            lineHeight: '60px',
-            textAlign: 'center',
-            background: '#E6E9FF',
-        }}
-    >
-        Footer
-    </div>
+  <div
+    style={{
+      height: "60px",
+      lineHeight: "60px",
+      textAlign: "center",
+      background: "#E6E9FF"
+    }}
+  >
+    Footer
+  </div>
 );
 
 export default class LayoutDemo extends React.Component {
-    render() {
-        return (
-            <Layout direction="row">
-              <Theme config={{ [Widget.Aside]: { backgroundColor: '#CCD4FF' } }}>
-                <Aside collapsible breakpoint={'lg'}>
-                    <div>Aside</div>
-                </Aside>
-              </Theme>
-                <Layout>
-                    <Header>{header}</Header>
-                    <Content>{content}</Content>
-                    <Footer>{footer}</Footer>
-                </Layout>
-            </Layout>
-        );
-    }
+  render() {
+    return (
+      <Layout direction="row">
+        <Aside collapsible breakpoint={"lg"}>
+          <div>Aside</div>
+        </Aside>
+        <Layout>
+          <Header>{header}</Header>
+          <Content>{content}</Content>
+          <Footer>{footer}</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
