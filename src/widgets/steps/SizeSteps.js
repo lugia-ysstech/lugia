f9519aa5c0d936c6ed466d05496db0fa0a4050de
jelly@pudding <liang_guodong@126.com>
2020-01-20 18:35:11 +0800

    Update    update steps demo

--- src/widgets/steps/SizeSteps.js
+++ src/widgets/steps/SizeSteps.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Steps, Button, Theme } from '@lugia/lugia-web';
-import Widget from '@lugia/lugia-web/dist/consts';
+import React from "react";
+import styled from "styled-components";
+import { Steps, Button, Theme } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts";
 const Step = Steps.Step;
 const Wrapper = styled.div`
   margin-bottom: 50px;
 `;
 export default class SizeSteps extends React.Component<any, any> {
   render() {
-    const view = {
-      [Widget.Steps]: {
-        width: 600
-      },
-    };
     return (
-      <Theme config={view}>
+      <div>
         <Wrapper>
-          <Steps  stepType="simple" size="mini">
+          <Steps stepType="simple" size="mini">
             <Step title="step1" stepStatus="finish" content="content1" />
             <Step title="step2" stepStatus="process" content="content2" />
             <Step title="step3" stepStatus="next" content="content3" />
           </Steps>
         </Wrapper>
         <Wrapper>
-          <Steps  stepType="simple" size="normal">
+          <Steps stepType="simple" size="normal">
             <Step title="step1" stepStatus="finish" content="content1" />
             <Step title="step2" stepStatus="process" content="content2" />
             <Step title="step3" stepStatus="next" content="content3" />
           </Steps>
         </Wrapper>
-      </Theme>
+      </div>
     );
   }
 }
