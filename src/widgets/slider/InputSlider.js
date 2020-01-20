09ad5ac5fff6913982279af34d1a266536630abc
cuicuiwang <1623330976@qq.com>
2020-01-20 14:53:42 +0800

    update:滑块 demo 展示更新

--- src/widgets/slider/InputSlider.js
+++ src/widgets/slider/InputSlider.js
@@ -1,40 +1,47 @@
-import React from 'react';
-import { Slider,NumberInput } from '@lugia/lugia-web';
-import styled from 'styled-components';
-export const DemoItem=styled.div`
-  display:inline-block;
-  padding:0 20px 20px 0;
+import React from "react";
+import { Slider, NumberInput, Theme } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts";
+import styled from "styled-components";
+export const DemoItem = styled.div`
+  display: inline-block;
+  padding: 0 20px 20px 0;
 `;
 export default class InputSlider extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      value:5,
-      maxValue:30,
-      minValue:0,
+    this.state = {
+      value: 5,
+      maxValue: 30,
+      minValue: 0
     };
   }
   onChange = params => {
-    const {newValue}=params;
-    this.setState({value:newValue});
+    const { newValue } = params;
+    this.setState({ value: newValue });
   };
   render() {
-    const {value,maxValue,minValue}=this.state;
+    const { value, maxValue, minValue } = this.state;
     return (
       <React.Fragment>
         <DemoItem>
-          <Slider 
-            value={value} 
-            maxValue={maxValue} 
-            minValue={minValue} 
-            onChange={this.onChange}
-          /> 
+          <Theme
+            config={{
+              [Widget.Slider]: { Container: { normal: { width: 300 } } }
+            }}
+          >
+            <Slider
+              value={value}
+              maxValue={maxValue}
+              minValue={minValue}
+              onChange={this.onChange}
+            />
+          </Theme>
         </DemoItem>
-        <NumberInput 
+        <NumberInput
           value={value}
-          max={maxValue} 
-          min={minValue} 
-          onChange={this.onChange} 
+          max={maxValue}
+          min={minValue}
+          onChange={this.onChange}
         />
       </React.Fragment>
     );
--- src/widgets/slider/VerticalSlider.js
+++ src/widgets/slider/VerticalSlider.js
@@ -1,47 +1,51 @@
-import React from 'react';
-import { Slider } from '@lugia/lugia-web';
-import styled from 'styled-components';
-export const DemoList=styled.span`
-  display:inline-block;
-  padding-right:30px;
+import React from "react";
+import { Slider, Theme } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts";
+import styled from "styled-components";
+export const DemoList = styled.span`
+  display: inline-block;
+  padding-right: 30px;
 `;
 export default class IconSlider extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <DemoList>
-          <Slider defaultValue={10} vertical tips/>
-        </DemoList>
-        <DemoList>
-          <Slider
-            defaultValue={[10, 20]}
-            vertical
-          />
-        </DemoList>
-        <DemoList>
-          <Slider
+        <Theme
+          config={{
+            [Widget.Slider]: { Container: { normal: { width: 300 } } }
+          }}
+        >
+          <DemoList>
+            <Slider defaultValue={10} vertical tips />
+          </DemoList>
+          <DemoList>
+            <Slider defaultValue={[10, 20]} vertical />
+          </DemoList>
+          <DemoList>
+            <Slider
               defaultValue={30}
               tips
               vertical
               marks={{
                 0: {
-                  text: '0℃'
+                  text: "0℃"
                 },
                 30: {
-                  text: '30℃'
+                  text: "30℃"
                 },
                 70: {
-                  text: '70℃'
+                  text: "70℃"
                 },
                 100: {
-                  text: '100℃',
+                  text: "100℃",
                   style: {
-                    color: 'red'
+                    color: "red"
                   }
                 }
               }}
             />
-        </DemoList>
+          </DemoList>
+        </Theme>
       </React.Fragment>
     );
   }
