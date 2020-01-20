918f7717930063231bc4600804042ab58a8bbe0c
jelly@pudding <liang_guodong@126.com>
2020-01-20 18:27:09 +0800

    Update    update pagination demo

--- src/widgets/pagination/JumperDemo.js
+++ src/widgets/pagination/JumperDemo.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Pagination} from '@lugia/lugia-web';
+import React from "react";
+import { Pagination } from "@lugia/lugia-web";
 
 export default class PaginationDemo extends React.Component {
   onChange = pageNumber => {
@@ -7,7 +7,12 @@ export default class PaginationDemo extends React.Component {
   };
   render() {
     return (
-      <Pagination showQuickJumper defaultCurrent={2} total={500} onChange={this.onChange} />
+      <Pagination
+        showQuickJumper
+        defaultCurrent={2}
+        total={500}
+        onChange={this.onChange}
+      />
     );
   }
 }
