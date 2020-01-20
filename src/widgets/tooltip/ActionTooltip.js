21d3fca82836dd291c4216711ac80997f9b8ea3e
jelly@pudding <liang_guodong@126.com>
2020-01-20 18:39:43 +0800

    Update    update tooltip demo

--- src/widgets/tooltip/ActionTooltip.js
+++ src/widgets/tooltip/ActionTooltip.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Tooltip, Theme, Button, Input } from '@lugia/lugia-web';
-import Widget from '@lugia/lugia-web/dist/consts';
-import styled from 'styled-components';
-import { Popconfirm } from '../popconfirm/ActionPopconfirm';
+import React from "react";
+import { Tooltip, Theme, Button, Input } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts";
+import styled from "styled-components";
+import { Popconfirm } from "../popconfirm/ActionPopconfirm";
 
 const Direction = styled(Button)``;
 const Wrapper = styled.div``;
@@ -12,36 +12,24 @@ const PopWrapper = styled.div`
 `;
 export default class ActionTooltip extends React.Component<any, any> {
   render() {
-    const text = 'this is title ';
+    const text = "this is title ";
     return (
       <Wrapper>
         <PopWrapper>
-          <Tooltip
-            title={text}
-            action={'click'}
-            placement="top"
-          >
+          <Tooltip title={text} action={"click"} placement="top">
             <Direction type="primary">鼠标点击</Direction>
           </Tooltip>
         </PopWrapper>
         <PopWrapper>
-          <Tooltip
-            title={text}
-            action={'hover'}
-            placement="top"
-          >
+          <Tooltip title={text} action={"hover"} placement="top">
             <Direction type="primary">鼠标移入</Direction>
           </Tooltip>
         </PopWrapper>
         <PopWrapper>
-        <Tooltip
-          title={text}
-          action={'focus'}
-          placement="top"
-        >
-          <Input placeholder={'聚焦弹出'} />
-        </Tooltip>
-      </PopWrapper>
+          <Tooltip title={text} action={"focus"} placement="top">
+            <Input placeholder={"聚焦弹出"} />
+          </Tooltip>
+        </PopWrapper>
       </Wrapper>
     );
   }
