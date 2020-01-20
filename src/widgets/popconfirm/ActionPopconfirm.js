6c6579d0fe478c9e1834e10cc84038260f402fe4
jelly@pudding <liang_guodong@126.com>
2020-01-20 18:30:27 +0800

    Update    update popconfirm demo

--- src/widgets/popconfirm/ActionPopconfirm.js
+++ src/widgets/popconfirm/ActionPopconfirm.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Popconfirm, Theme, Button, Input } from '@lugia/lugia-web';
-import styled from 'styled-components';
+import React from "react";
+import { Popconfirm, Theme, Button, Input } from "@lugia/lugia-web";
+import styled from "styled-components";
 
 const Direction = styled(Button)``;
 const Wrapper = styled.div``;
@@ -9,32 +9,23 @@ const PopWrapper = styled.div`
   display: inline-block;
 `;
 export default class ActionPopconfirm extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = { value: '' };
-  }
-
-  onChange = ({ newValue: value }: any) => {
-    this.setState({ value });
-  };
-
   render() {
-    const text = '确定删除这个选项吗?';
+    const text = "确定删除这个选项吗?";
     return (
       <Wrapper>
         <PopWrapper>
-          <Popconfirm placement="top" title={text} action={'click'}>
+          <Popconfirm placement="top" title={text} action={"click"}>
             <Direction type="primary">鼠标点击</Direction>
           </Popconfirm>
         </PopWrapper>
         <PopWrapper>
-          <Popconfirm placement="top" title={text} action={'hover'}>
+          <Popconfirm placement="top" title={text} action={"hover"}>
             <Direction type="primary">鼠标移入</Direction>
           </Popconfirm>
         </PopWrapper>
         <PopWrapper>
-          <Popconfirm title={text} action={'focus'} placement="top">
-            <Input placeholder={'聚焦弹出'} />
+          <Popconfirm title={text} action={"focus"} placement="top">
+            <Input placeholder={"聚焦弹出"} />
           </Popconfirm>
         </PopWrapper>
       </Wrapper>
