bb3dffdb8740e466c6f48a048211d777520deeeb
jelly@pudding <liang_guodong@126.com>
2020-01-20 18:59:04 +0800

    Update    update progress demo

--- src/widgets/progress/config.js
+++ src/widgets/progress/config.js
@@ -11,10 +11,6 @@ module.exports =  {
         title: '内容内置',
         desc: '内容内置的进图条，节省空间'
     },
-    ThemeLineDemo: {
-        title: '配置主题',
-        desc: '可配置主题的线性进度条，可配置 color width height'
-    },
     ChangeLineDemo: {
         title: '动态展示',
         desc: '可以动的进度条'
@@ -31,16 +27,8 @@ module.exports =  {
         title: '小型圆形进度条',
         desc: '小型圆形进度条，节省空间'
     },
-    ThemeCircleDemo: {
-        title: '圆形进度条配置主题',
-        desc: '可配置主题圆形进度条，可配置 color'
-    },
     DashboardDemo: {
         title: '仪表盘',
         desc: '仪表盘样式的进度条'
     },
-    ThemeDashboardDemo: {
-        title: '仪表盘主题',
-        desc: '可配置主题仪表盘样式的进度条，可配置 color'
-    }
 };
