5dd975ad63d0ffdef9dd28afa9e515ac97e1d8a3
szfeng <360998950@qq.com>
2020-01-21 13:03:20 +0800

    cascader：demo修改

--- src/widgets/cascader/DefaultCascader.js
+++ src/widgets/cascader/DefaultCascader.js
@@ -3,69 +3,69 @@
  * create by szfeng
  *
  */
-import React from 'react';
-import { Cascader, Theme } from '@lugia/lugia-web';
-import Widget from '@lugia/lugia-web/dist/consts';
+import React from "react";
+import { Cascader, Theme } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts";
 
 const data = [
   {
-    value: 'Lugia Design',
-    text: 'Lugia Design',
-    icon: 'lugia-icon-financial_add_pic'
+    value: "Lugia Design",
+    text: "Lugia Design",
+    icon: "lugia-icon-financial_add_pic"
   },
   {
-    value: 'Lugia-mega',
-    text: 'Lugia-mega',
+    value: "Lugia-mega",
+    text: "Lugia-mega"
   },
   {
-    value: 'Components',
-    text: 'Components',
+    value: "Components",
+    text: "Components",
     children: [
       {
-        value: 'General',
-        text: 'General',
+        value: "General",
+        text: "General",
         children: [
           {
-            value: 'Button',
-            text: 'Button',
-            icon: 'lugia-icon-financial_add_pic'
+            value: "Button",
+            text: "Button",
+            icon: "lugia-icon-financial_add_pic"
           },
-          { value: 'Icon', text: 'Icon', icon: 'lugia-icon-financial_archive' }
+          { value: "Icon", text: "Icon", icon: "lugia-icon-financial_archive" }
         ]
       },
 
       {
-        value: 'Layout',
-        text: 'Layout',
+        value: "Layout",
+        text: "Layout",
         children: [
-          { value: 'Grid', text: 'Grid' },
-          { value: 'Layout', text: 'Layout' }
+          { value: "Grid", text: "Grid" },
+          { value: "Layout", text: "Layout" }
         ]
       },
 
       {
-        value: 'Navigation',
-        text: 'Navigation',
+        value: "Navigation",
+        text: "Navigation",
         children: [
-          { value: 'Affix', text: 'Affix' },
-          { value: 'Breadcrumb', text: 'Breadcrumb' },
-          { value: 'Dropdown', text: 'Dropdown' },
-          { value: 'Menu', text: 'Menu' },
-          { value: 'Pagination', text: 'Pagination' },
-          { value: 'Steps', text: 'Steps' }
+          { value: "Affix", text: "Affix" },
+          { value: "Breadcrumb", text: "Breadcrumb" },
+          { value: "Dropdown", text: "Dropdown" },
+          { value: "Menu", text: "Menu" },
+          { value: "Pagination", text: "Pagination" },
+          { value: "Steps", text: "Steps" }
         ]
       },
 
       {
-        value: 'Data Entry',
-        text: 'Data Entry',
+        value: "Data Entry",
+        text: "Data Entry",
         children: [
-          { value: 'AutoComplete', text: 'AutoComplete' },
-          { value: 'Cascader', text: 'Cascader' },
-          { value: 'Checkbox', text: 'Checkbox' },
-          { value: 'DatePicker', text: 'DatePicker' },
-          { value: 'Form', text: 'Form' },
-          { value: 'Input', text: 'Input' }
+          { value: "AutoComplete", text: "AutoComplete" },
+          { value: "Cascader", text: "Cascader" },
+          { value: "Checkbox", text: "Checkbox" },
+          { value: "DatePicker", text: "DatePicker" },
+          { value: "Form", text: "Form" },
+          { value: "Input", text: "Input" }
         ]
       }
     ]
@@ -78,15 +78,19 @@ export default class extends React.Component<any, any> {
       <div>
         <Theme
           config={{
-            [Widget.Cascader]: { width: 300 }
+            [Widget.Cascader]: {
+              Container: {
+                normal: { width: 300 }
+              }
+            }
           }}
         >
           <Cascader
             data={data}
-            placeholder={'请选择'}
-            action={'hover'}
-            separator={'*'}
-            defaultValue={'Components*Data Entry*Cascader'}
+            placeholder={"请选择"}
+            action={"hover"}
+            separator={"*"}
+            defaultValue={"Components*Data Entry*Cascader"}
           />
         </Theme>
       </div>
