a4397019147b6109b52c688e3e9ece1b2728a21b
szfeng <360998950@qq.com>
2020-01-21 13:02:56 +0800

    breadcrumb：demo修改

--- src/widgets/breadcrumb/RoutesBreadcrumb.js
+++ src/widgets/breadcrumb/RoutesBreadcrumb.js
@@ -1,30 +1,34 @@
 /**
  * create by szfeng
  */
-import React from 'react';
-import { Breadcrumb } from '@lugia/lugia-web';
+import React from "react";
+import { Breadcrumb } from "@lugia/lugia-web";
 
 const routes = [
   {
-    path: 'index',
-    title: '首页'
+    path: "index",
+    title: "首页"
   },
   {
-    path: 'first',
-    title: '一级面包屑'
+    path: "first",
+    title: "一级面包屑",
+    icons: {
+      prefixIconClass: "lugia-icon-logo_chrome",
+      suffixIconClass: "lugia-icon-logo_dropbox"
+    }
   },
   {
-    path: 'second:id',
-    title: '二级面包屑:id'
+    path: "second:id",
+    title: "二级面包屑:id"
   },
   {
-    path: 'third',
-    title: '四级面包屑'
+    path: "third",
+    title: "四级面包屑"
   }
 ];
 
 export default class RoutesBreadcrumb extends React.Component<any, any> {
   render() {
-    return <Breadcrumb routes={routes} params={{ id: 'params' }} />;
+    return <Breadcrumb routes={routes} params={{ id: "params" }} />;
   }
 }
