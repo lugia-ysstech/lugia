63ff517404b90ead517e3ddd62c79040b18fd3a3
jelly@pudding <liang_guodong@126.com>
2020-01-21 10:23:59 +0800

    Update    update time-line demo

--- src/widgets/time-line/DescriptionTimeLine.js
+++ src/widgets/time-line/DescriptionTimeLine.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { TimeLine, Theme, Button, Icon, tabs } from '@lugia/lugia-web';
-import styled from 'styled-components';
+import React from "react";
+import { TimeLine } from "@lugia/lugia-web";
+import styled from "styled-components";
 const TimeLineItem = TimeLine.TimeLineItem;
 const Wrapper = styled.div`
   display: inline-block;
@@ -8,15 +8,15 @@ const Wrapper = styled.div`
 export default class DescriptionTimeLine extends React.Component<any, any> {
   render() {
     return (
-          <Wrapper>
-            <TimeLine>
-              <TimeLineItem time="2018-01-01" description={'description111'} />
-              <TimeLineItem time="2018-01-02" description={'description222'} />
-              <TimeLineItem time="2018-01-03" description={'description333'} />
-              <TimeLineItem time="2018-01-04" description={'description444'} />
-              <TimeLineItem time="2018-01-05" description={'description555'} />
-            </TimeLine>
-          </Wrapper>
+      <Wrapper>
+        <TimeLine>
+          <TimeLineItem time="2018-01-01" description={"description111"} />
+          <TimeLineItem time="2018-01-02" description={"description222"} />
+          <TimeLineItem time="2018-01-03" description={"description333"} />
+          <TimeLineItem time="2018-01-04" description={"description444"} />
+          <TimeLineItem time="2018-01-05" description={"description555"} />
+        </TimeLine>
+      </Wrapper>
     );
   }
 }
