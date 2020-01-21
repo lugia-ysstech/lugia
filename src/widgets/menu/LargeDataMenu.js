8a087a19ace36fed6cc03309ee4e6a4c66c8ade6
szfeng <360998950@qq.com>
2020-01-21 13:04:12 +0800

    menu：demo update

--- src/widgets/menu/LargeDataMenu.js
+++ src/widgets/menu/LargeDataMenu.js
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import { Menu, Theme } from '@lugia/lugia-web';
-import Widget from '@lugia/lugia-web/dist/consts';
-import styled from 'styled-components';
+import * as React from "react";
+import { Menu, Theme } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts";
+import styled from "styled-components";
 
 const MenuWrap = styled.div`
-  box-shadow:0px 0px 6px rgba(51,51,51,0.2);
+  box-shadow: 0px 0px 6px rgba(51, 51, 51, 0.2);
   display: inline-block;
   margin: 20px;
   border-radius: 4px;
@@ -12,17 +12,25 @@ const MenuWrap = styled.div`
 
 const data = [];
 for (let i = 0; i < 200000; i++) {
-  data.push({ text: i, value: i, disabled: false });
+  data.push({ text: `${i}`, value: `${i}`, disabled: false });
 }
 
 export default class extends React.Component<any, any> {
   render() {
-    const config = { [Widget.Menu]: { width: 300 } };
+    const config = {
+      [Widget.Menu]: {
+        Container: {
+          normal: {
+            width: 200
+          }
+        }
+      }
+    };
     return (
       <div>
         <Theme config={config}>
           <MenuWrap>
-            <Menu checkedCSS={'checkbox'} data={data} mutliple />
+            <Menu checkedCSS={"checkbox"} data={data} mutliple />
           </MenuWrap>
         </Theme>
       </div>
