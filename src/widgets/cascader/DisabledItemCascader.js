4c7fc712e5bc55b79087d63c53ebb6620fdc18de
szfeng <360998950@qq.com>
2020-01-21 13:03:20 +0800

    cascader：demo修改

--- src/widgets/cascader/DisabledItemCascader.js
+++ src/widgets/cascader/DisabledItemCascader.js
@@ -3,35 +3,35 @@
  * create by szfeng
  *
  */
-import React from 'react';
-import { Cascader, Theme } from '@lugia/lugia-web';
-import Widget from '@lugia/lugia-web/dist/consts';
+import React from "react";
+import { Cascader, Theme } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts";
 
 const data = [
   {
-    value: 'Navigation',
-    text: 'Navigation',
+    value: "Navigation",
+    text: "Navigation",
     children: [
-      { value: 'Affix', text: 'Affix' },
-      { value: 'Breadcrumb', text: 'Breadcrumb', disabled: true },
-      { value: 'Dropdown', text: 'Dropdown' },
-      { value: 'Menu', text: 'Menu', disabled: true },
-      { value: 'Pagination', text: 'Pagination' },
-      { value: 'Steps', text: 'Steps' }
+      { value: "Affix", text: "Affix" },
+      { value: "Breadcrumb", text: "Breadcrumb", disabled: true },
+      { value: "Dropdown", text: "Dropdown" },
+      { value: "Menu", text: "Menu", disabled: true },
+      { value: "Pagination", text: "Pagination" },
+      { value: "Steps", text: "Steps" }
     ]
   },
 
   {
-    value: 'Data Entry',
-    text: 'Data Entry',
+    value: "Data Entry",
+    text: "Data Entry",
     disabled: true,
     children: [
-      { value: 'AutoComplete', text: 'AutoComplete' },
-      { value: 'Cascader', text: 'Cascader' },
-      { value: 'Checkbox', text: 'Checkbox' },
-      { value: 'DatePicker', text: 'DatePicker' },
-      { value: 'Form', text: 'Form' },
-      { value: 'Input', text: 'Input' }
+      { value: "AutoComplete", text: "AutoComplete" },
+      { value: "Cascader", text: "Cascader" },
+      { value: "Checkbox", text: "Checkbox" },
+      { value: "DatePicker", text: "DatePicker" },
+      { value: "Form", text: "Form" },
+      { value: "Input", text: "Input" }
     ]
   }
 ];
@@ -42,14 +42,18 @@ export default class extends React.Component<any, any> {
       <div>
         <Theme
           config={{
-            [Widget.Cascader]: { width: 300 }
+            [Widget.Cascader]: {
+              Container: {
+                normal: { width: 300 }
+              }
+            }
           }}
         >
           <Cascader
             data={data}
-            placeholder={'请选择'}
-            separator={'/'}
-            action={'hover'}
+            placeholder={"请选择"}
+            separator={"/"}
+            action={"hover"}
           />
         </Theme>
       </div>
