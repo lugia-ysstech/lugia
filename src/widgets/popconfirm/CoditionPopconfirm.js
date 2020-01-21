c468275190a659b7c85974981199c3290be504f0
jelly@pudding <liang_guodong@126.com>
2020-01-21 10:30:58 +0800

    Update    update popconfirm demo

--- src/widgets/popconfirm/CoditionPopconfirm.js
+++ src/widgets/popconfirm/CoditionPopconfirm.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import {
-  Popconfirm,
-  Theme,
-  Button,
-  notification,
-  Switch
-} from '@lugia/lugia-web';
-import styled from 'styled-components';
+import React from "react";
+import { Popconfirm, Button, notification, Switch } from "@lugia/lugia-web";
+import styled from "styled-components";
 const PopWrapper = styled.div`
   margin-right: 10px;
-  display:inline-block;
+  display: inline-block;
 `;
 const Direction = styled(Button)``;
 export default class CoditionPopconfirm extends React.Component<any, any> {
@@ -25,12 +19,12 @@ export default class CoditionPopconfirm extends React.Component<any, any> {
 
   confirm = () => {
     this.setState({ visible: false });
-    notification.success({ title: '操作成功 ' });
+    notification.success({ title: "操作成功 " });
   };
 
   cancel = () => {
     this.setState({ visible: false });
-    notification.error({ title: '取消操作' });
+    notification.error({ title: "取消操作" });
   };
 
   handleVisibleChange = (visible: Object) => {
@@ -47,22 +41,24 @@ export default class CoditionPopconfirm extends React.Component<any, any> {
 
   render() {
     return (
-      <div >
+      <div>
         <PopWrapper>
-        <Switch defaultChecked onChange={this.changeCondition} /></PopWrapper>
-        <PopWrapper> 
-        <Popconfirm
-          placement="top"
-          title="确定要删除吗?"
-          visible={this.state.visible}
-          onVisibleChange={this.handleVisibleChange}
-          onConfirm={this.confirm}
-          onCancel={this.cancel}
-          okText="确定"
-          cancelText="取消"
-        >
-          <Direction type="primary">删除任务</Direction>
-        </Popconfirm></PopWrapper>
+          <Switch defaultChecked onChange={this.changeCondition} />
+        </PopWrapper>
+        <PopWrapper>
+          <Popconfirm
+            placement="top"
+            title="确定要删除吗?"
+            visible={this.state.visible}
+            onVisibleChange={this.handleVisibleChange}
+            onConfirm={this.confirm}
+            onCancel={this.cancel}
+            okText="确定"
+            cancelText="取消"
+          >
+            <Direction type="primary">删除任务</Direction>
+          </Popconfirm>
+        </PopWrapper>
       </div>
     );
   }
