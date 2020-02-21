8a66fb0262cb6cef5c1bc12891b1578af1e51ea7
grguang <945455382@qq.com>
2020-02-21 17:18:36 +0800

    Update:     * button theme demo;

--- src/widgets/button/ThemeDemo.js
+++ src/widgets/button/ThemeDemo.js
@@ -1,26 +1,28 @@
-import * as React from 'react';
-import {Button,Theme} from '@lugia/lugia-web';
-import Widget from '@lugia/lugia-web/dist/consts/index';
+import * as React from "react";
+import { Button, Theme } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts/index";
 
 export default class ButtonDemo extends React.Component<any, any> {
-
-    render() {
-        const view = {
-            [Widget.Button]: {
-                width: 100,
-            },
-            register: {
-                width: 300,
-                margin: 10,
-                color: 'red',
-            },
-        };
-        return (
-            <div>
-                <Theme config={view}>
-                    <Button viewClass="register">Button</Button>
-                </Theme>
-            </div>
-        );
-    }
+  render() {
+    const view = {
+      [Widget.Button]: {
+        Container: {
+          normal: {
+            width: 100,
+            height: 50,
+            padding: 9,
+            margin: 10,
+            background: { color: "orange" }
+          }
+        }
+      }
+    };
+    return (
+      <div>
+        <Theme config={view}>
+          <Button>Button</Button>
+        </Theme>
+      </div>
+    );
+  }
 }
