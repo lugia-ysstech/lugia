cbf38ddd3599e1a784e16e8ee9c0f5f5eb690537
jelly@pudding <liang_guodong@126.com>
2020-04-14 16:57:43 +0800

    Update    update Pagination demo

--- src/widgets/pagination/BlockListDemo.js
+++ src/widgets/pagination/BlockListDemo.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Pagination } from "@lugia/lugia-web";
-
+import styled from "styled-components";
+const Wrapper = styled.div`
+  height: 30px;
+`;
 export default class PaginationDemo extends React.Component {
   onShowSizeChange = (current, pageSize) => {
     console.log(current, pageSize);
@@ -8,6 +11,7 @@ export default class PaginationDemo extends React.Component {
   render() {
     return (
       <div>
+        <Wrapper>分页 展示分页列表和快速跳转</Wrapper>
         <Pagination
           blockList={["Page", "PageInput"]}
           isShowTotalData
@@ -18,7 +22,8 @@ export default class PaginationDemo extends React.Component {
           onChange={this.onChange}
           onShowSizeChange={this.onShowSizeChange}
         />
-        <br/>
+        <br />
+        <Wrapper>分页 展示分页列表，总计数据和快速跳转</Wrapper>
         <Pagination
           blockList={["Page", "Total", "PageInput"]}
           isShowTotalData
@@ -29,7 +34,8 @@ export default class PaginationDemo extends React.Component {
           onChange={this.onChange}
           onShowSizeChange={this.onShowSizeChange}
         />
-        <br/>
+        <br />
+        <Wrapper>分页 展示分页列表，快速跳转，总计数据和展示条数下拉框</Wrapper>
         <Pagination
           blockList={["Page", "PageInput", "Total", "PageSize"]}
           isShowTotalData
