97de3f53a4ce9a83f4aa34b50d869ebdbdf11de9
Shine_Lee <wangyi_lixian@163.com>
2020-08-20 19:16:37 +0800

    Add: tutorial center

---
+++ src/tutorial-center/feedback.js
@@ -0,0 +1,417 @@
+import React, { Component, Fragment } from "react";
+import styled from "styled-components";
+import { Theme, Grid, Radio, Upload, Button, Input } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts/index";
+import { getBorderRadius, getBorder } from "@lugia/theme-utils";
+import Header from "../components/header";
+
+import banner from "./img/banner-background.png";
+import { linkToUrl } from "../support/commonMethods";
+
+const { Row, Col } = Grid;
+const { Group: RadioGroup, Button: RadioButton } = Radio;
+const { Textarea } = Input;
+
+const BannerImg = styled.div`
+  width: 100%;
+  height: 520px;
+  background:url("${banner}") no-repeat;
+  background-size: auto 520px;
+  background-position: center center;
+  position:absolute;
+  top:0;
+  z-index:-1;
+  &:after {
+    display: inline-block;
+    content: "";
+    width: 100%;
+    height: 170px;
+    background: #F6FAFB;
+    position: absolute;
+    bottom: -170px;
+  }
+`;
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 20px;
+`;
+
+const DialogWrap = styled.div`
+  width: 100%;
+  display: grid;
+  place-items: center;
+`;
+const DialogWrapInner = styled.div`
+  width: 700px;
+  background: #ffffff;
+  box-shadow: 0 2px 10px 0 rgba(77, 104, 255, 0.19);
+`;
+const ContentWrap = styled.div`
+  width: 100%;
+  display: grid;
+  place-items: center;
+`;
+
+const DialogTitleWrap = styled(ContentWrap)`
+  height: 70px;
+`;
+const DialogTitle = styled.div`
+  font-family: PingFangSC-Regular;
+  font-size: 16px;
+  font-weight: bold;
+  color: #333333;
+  text-align: center;
+`;
+
+const DialogContentWrap = styled(ContentWrap)``;
+const UserInputAreaWrap = styled.div`
+  width: 100%;
+  padding: 0 50px;
+`;
+const UserInputArea = styled.div`
+  width: 100%;
+`;
+const UserInputTitle = styled.div`
+  width: 100%;
+  line-height: 20px;
+  margin: 24px 0 6px 0;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  color: #202700;
+`;
+const StarWrap = styled.span`
+  display: inline-block;
+  margin-right: 2px;
+  font-size: 18px;
+  color: #f22735;
+  transform: translateY(5px);
+`;
+const UploadFileWrap = styled.div`
+  width: 100%;
+`;
+const UploadTitle = styled.div`
+  line-height: 20px;
+  margin: 24px 0 6px 0;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  color: #50575d;
+`;
+
+const DialogFooter = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const radioGroupTheme = {
+  [Widget.RadioGroup]: {
+    Container: {
+      normal: {
+        width: 600,
+        height: 32,
+        borderRadius: getBorderRadius(4),
+        background: { color: "#fff" }
+      }
+    },
+    CheckButton: {
+      CheckButtonChecked: {
+        normal: {
+          width: 150,
+          height: 32,
+          border: getBorder({ color: "#4D68FF", width: 1, style: "solid" }),
+          color: "#fff",
+          font: { fontSize: 14 }
+        }
+      },
+      CheckButtonUnChecked: {
+        normal: {
+          width: 150,
+          height: 32,
+          font: { fontSize: 14 }
+        }
+      }
+    }
+  }
+};
+const textareaTheme = {
+  [Widget.Textarea]: {
+    Container: {
+      normal: {
+        width: 600,
+        height: 60
+      }
+    }
+  }
+};
+const buttonTheme = {
+  [Widget.Button]: {
+    Container: {
+      normal: {
+        width: 160,
+        height: 32,
+        background: {
+          image: "linear-gradient(90deg, #4D68FF 0%, #8093FF 100%)"
+        },
+        margin: {
+          right: 8,
+          left: 8
+        }
+      },
+      disabled: {
+        color: "#FFFFFF",
+        background: {
+          color: "#D8D8D8",
+          image: "none"
+        }
+      }
+    }
+  }
+};
+const cancelButtonTheme = {
+  [Widget.Button]: {
+    Container: {
+      normal: {
+        width: 160,
+        height: 32,
+        margin: {
+          right: 8,
+          left: 8
+        }
+      }
+    }
+  }
+};
+
+export default class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      radioValue: "0",
+      textValue: {
+        0: { value: "", title: "您找不到什么教程呢？" },
+        1: {
+          value: "",
+          title: "您看不懂什么教程呢？",
+          placeholder: true,
+          desc: true,
+          descValue: "",
+          descTitle: "您能说一下具体哪里看不懂嘛？"
+        },
+        2: {
+          value: "",
+          title: "您发现哪个教程有错误呢？",
+          placeholder: true,
+          desc: true,
+          descValue: "",
+          descTitle: "您能说一下哪里有错误吗？"
+        },
+        3: { value: "", title: "请输入您的反馈" }
+      }
+    };
+  }
+
+  onRadioGroupChange = obj => {
+    const { newValue } = obj;
+    this.setState({
+      radioValue: newValue
+    });
+  };
+
+  getFormatStateData = () => {
+    const { textValue } = this.state;
+    let dataIndexArr = Object.keys(textValue);
+    let dataArr = [];
+    const len = dataIndexArr.length;
+    for (let i = 0; i < len; i++) {
+      dataArr.push(textValue[dataIndexArr[i]]);
+    }
+    return dataArr;
+  };
+
+  handleTextareaChange = (index, value, desc = false) => {
+    const { textValue } = this.state;
+    const oldValue = textValue[index];
+    const chosenValue = desc ? "descValue" : "value";
+    this.setState({
+      textValue: {
+        ...textValue,
+        [index]: { ...oldValue, [chosenValue]: value }
+      }
+    });
+  };
+  getUserInputArea = () => {
+    const { radioValue } = this.state;
+    const data = this.getFormatStateData();
+    return (
+      data &&
+      data.map((item, index) => {
+        const {
+          value,
+          title,
+          placeholder = false,
+          desc = false,
+          descTitle = "",
+          descValue = ""
+        } = item;
+        if (radioValue == index) {
+          return (
+            <UserInputArea>
+              <UserInputTitle>
+                <StarWrap>*</StarWrap>
+                {title}
+              </UserInputTitle>
+              <Theme config={textareaTheme}>
+                <Textarea
+                  placeholder={
+                    placeholder && "请把教程名称或教程链接粘贴在这里"
+                  }
+                  value={value}
+                  onChange={({ newValue }) =>
+                    this.handleTextareaChange(index, newValue)
+                  }
+                />
+              </Theme>
+              {desc && (
+                <Fragment>
+                  <UserInputTitle>{descTitle}</UserInputTitle>
+                  <Theme config={textareaTheme}>
+                    <Textarea
+                      placeholder="请把教程名称或教程链接粘贴在这里"
+                      value={descValue}
+                      onChange={({ newValue }) =>
+                        this.handleTextareaChange(index, newValue, desc)
+                      }
+                    />
+                  </Theme>
+                </Fragment>
+              )}
+            </UserInputArea>
+          );
+        }
+      })
+    );
+  };
+
+  hasAllNecessaryValue = () => {
+    let flag = false;
+    const data = this.getFormatStateData();
+    data &&
+      data.forEach(item => {
+        const { value } = item;
+        value || (flag = true);
+      });
+    return flag;
+  };
+
+  handleOkClick = () => {
+    const { textValue } = this.state;
+    console.log("textValue", textValue);
+  };
+
+  render() {
+    const uploadDefaultProps = {
+      areaType: "default",
+      inputId: "upload",
+      showFileList: true,
+      url: "http://localhost:7001/upload",
+      multiple: true,
+      isShowProgress: false,
+      defaultTips: {
+        uploadTips: "请选择或拖拽上传文件, 限制每个在20MB以内"
+      },
+      accessKey: ["uploadToken"],
+      beforeUpload: (file: Object) => {
+        return new Promise((resolve, reject) => {
+          request({
+            url: "http://localhost:7001/getToken",
+            method: "post",
+            dataType: "json",
+            data: { name: file.name },
+            onSuccess: res => {
+              if (res.code === 200) {
+                file.uploadToken = res.data;
+                resolve({ status: true, file });
+              } else {
+                reject();
+              }
+            },
+            onFail: res => {
+              reject();
+            }
+          });
+        });
+      },
+      data: {},
+      onChange: res => {},
+      onSuccess: (res, fileList) => {},
+      onComplete: res => {},
+      onProgress: res => {},
+      onFail: res => {}
+    };
+
+    return (
+      <Fragment>
+        <BannerImg />
+        <Row>
+          <Col span={1} md={{ span: 1 }} xl={{ span: 2 }} xxl={{ span: 3 }}>
+            <Wrapper> </Wrapper>
+          </Col>
+          <Col span={22} md={{ span: 22 }} xl={{ span: 20 }} xxl={{ span: 18 }}>
+            <Header />
+
+            <DialogWrap>
+              <DialogWrapInner>
+                <DialogTitleWrap>
+                  <DialogTitle>您要提交什么优化反馈呢？</DialogTitle>
+                </DialogTitleWrap>
+
+                <DialogContentWrap>
+                  <Theme config={radioGroupTheme}>
+                    <RadioGroup
+                      childType="button"
+                      onChange={this.onRadioGroupChange}
+                      defaultValue="0"
+                    >
+                      <RadioButton value="0">找不到教程</RadioButton>
+                      <RadioButton value="1">看不懂教程</RadioButton>
+                      <RadioButton value="2">教程有错误</RadioButton>
+                      <RadioButton value="3">其它</RadioButton>
+                    </RadioGroup>
+                  </Theme>
+                  <UserInputAreaWrap>
+                    {this.getUserInputArea()}
+
+                    <UploadFileWrap>
+                      <UploadTitle>附件</UploadTitle>
+                      <Upload {...uploadDefaultProps} />
+                    </UploadFileWrap>
+                  </UserInputAreaWrap>
+                </DialogContentWrap>
+
+                <DialogFooter>
+                  <Theme config={buttonTheme}>
+                    <Button
+                      type="primary"
+                      disabled={this.hasAllNecessaryValue()}
+                      onClick={this.handleOkClick}
+                    >
+                      确定
+                    </Button>
+                  </Theme>
+                  <Theme config={cancelButtonTheme}>
+                    <Button onClick={() => linkToUrl("/tutorial")}>取消</Button>
+                  </Theme>
+                </DialogFooter>
+              </DialogWrapInner>
+            </DialogWrap>
+          </Col>
+          <Col span={1} md={{ span: 1 }} xl={{ span: 2 }} xxl={{ span: 3 }}>
+            <Wrapper> </Wrapper>
+          </Col>
+        </Row>
+      </Fragment>
+    );
+  }
+}
---
+++ src/tutorial-center/tutorialCenter.js
@@ -0,0 +1,482 @@
+import React, { Component, Fragment } from "react";
+import styled from "styled-components";
+import { Grid, Theme, Input, Icon } from "@lugia/lugia-web";
+import Widget from "@lugia/lugia-web/dist/consts/index";
+import { getBorderRadius } from "@lugia/theme-utils";
+import Header from "../components/header";
+import { Footer } from "../components/footer";
+import OpreationProcess from "./operationProcess";
+
+import banner from "./img/banner-background.png";
+import basicConstructor from "./img/basic-constructor.png";
+import themeConfig from "./img/theme-config.png";
+import smartConfig from "./img/smart-config.png";
+import assistIllustrate from "./img/assist-illustrate.png";
+import advanceDev from "./img/advance-dev.png";
+import notebook from "./img/notebook.png";
+import notebookMail from "./img/notebook-mail.png";
+import { linkToUrl } from "../support/commonMethods";
+
+const { Row, Col } = Grid;
+
+const BannerImg = styled.div`
+  width: 100%;
+  height: 520px;
+  background:url("${banner}") no-repeat;
+  background-size: auto 520px;
+  background-position: center center;
+  position:absolute;
+  top:0;
+  z-index:-1;
+  &:after {
+    display: inline-block;
+    content: "";
+    width: 100%;
+    height: 170px;
+    background: #F6FAFB;
+    position: absolute;
+    bottom: -170px;
+  }
+`;
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 20px;
+`;
+
+const SearchWrap = styled.div`
+  width: 100%;
+  height: 440px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const SearchHeader = styled.div`
+  margin-bottom: 25px;
+  font-family: PingFangSC-Medium;
+  font-size: 36px;
+  font-weight: bold;
+  color: #50575d;
+  text-align: center;
+`;
+const Content = styled.div`
+  width: 100%;
+`;
+const ContentTitleWrap = styled.div`
+  width: 100%;
+  height: 100px;
+  margin-bottom: 8px;
+  display: grid;
+  place-items: center;
+  position: relative;
+`;
+const ContentTitle = styled.h2`
+  font-family: PingFangSC-Medium;
+  font-size: 24px;
+  color: #010156;
+  &:after {
+    display: inline-block;
+    content: "";
+    width: 36px;
+    height: 4px;
+    background: #4d68ff;
+    position: absolute;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
+
+const VideoCardWrap = styled.div`
+  width: 100%;
+  height: 0;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
+const VideoWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 10;
+
+  & > iframe {
+    width: 100%;
+    height: 100%;
+  }
+`;
+const VideoTitle = styled.div`
+  width: 100%;
+  margin-top: 16px;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  color: #50575d;
+  text-align: center;
+`;
+
+const OperationalProcessWrap = styled.div`
+  width: 100%;
+  height: 320px;
+  background: #fafafc;
+  display: grid;
+  place-items: center;
+  overflow-x: auto;
+  overflow-y: hidden;
+`;
+
+const FeatureReferenceWrap = styled.div`
+  width: 100%;
+  display: flex;
+`;
+const FeatureReferenceCradWrap = styled.div`
+  width: 20%;
+  display: flex;
+  justify-content: center;
+`;
+const FeatureReferenceCradInner = styled.div`
+  display: inline-block;
+`;
+const ImageWrap = styled.div`
+  width: 48px;
+  height: 48px;
+  background: url(${props => (props.bgImage ? props.bgImage : "")}) no-repeat;
+  background-size: contain;
+`;
+const FeatureReferenceCradTitle = styled.a`
+  display: block;
+  height: 22px;
+  line-height: 22px;
+  margin: 10px 0 12px 0;
+  font-family: PingFangSC-Medium;
+  font-size: 16px;
+  font-weight: bold;
+  color: #50575d;
+  text-align: left;
+`;
+const FeatureReferenceCradItem = styled.a`
+  display: block;
+  margin-top: 10px;
+  font-family: PingFangSC-Regular;
+  font-size: 14px;
+  color: #50575d;
+  text-align: left;
+  &:hover {
+    color: #4d68ff;
+  }
+`;
+
+const FeedbackWrap = styled.div`
+  width: 100%;
+`;
+const FeedbackCardWrap = styled.div`
+  width: 100%;
+  height: 130px;
+  padding: 0 20px;
+  display: grid;
+  place-items: center;
+  background: #d4e1ff30;
+`;
+const MailFeedbackCardWrap = styled(FeedbackCardWrap)`
+  background: #cae6ff30;
+`;
+const MailFeedbackCardWrapInner = styled.div`
+  width: 100%;
+  display: flex;
+`;
+const FeedbackImg = styled.div`
+  width: 42px;
+  height: 50px;
+  flex-shrink: 0;
+  background: url(${props => (props.bgImage ? props.bgImage : "")}) no-repeat;
+`;
+const FeedbackContent = styled.div`
+  margin-left: 18px;
+`;
+const FeedbackTitle = styled.a`
+  display: block;
+  height: 22px;
+  margin-bottom: 8px;
+  line-height: 22px;
+  font-family: PingFangSC-Medium;
+  font-size: 16px;
+  font-weight: bold;
+  color: #50575d;
+  &:hover {
+    color: #4d68ff;
+  }
+`;
+const FeedbackDesc = styled.div`
+  height: 20px;
+  line-height: 20px;
+  font-family: PingFangSC-Regular;
+  word-wrap: break-word;
+  font-size: 14px;
+  color: #747e90;
+`;
+
+const searchTheme = {
+  [Widget.Input]: {
+    Container: {
+      normal: {
+        width: 665,
+        height: 48,
+        borderRadius: getBorderRadius(24),
+        padding: {
+          left: 30
+        },
+        font: {
+          size: 16
+        }
+      }
+    },
+    Placeholder: {
+      normal: {
+        font: {
+          size: 16
+        }
+      }
+    },
+    InputSuffix: {
+      normal: {
+        margin: {
+          right: 20
+        },
+        font: {
+          size: 16
+        }
+      }
+    }
+  }
+};
+
+const featureReferenceData = [
+  {
+    image: basicConstructor,
+    title: "基础搭建",
+    item: [
+      { itemTitle: "创建项目", itemUrl: "" },
+      { itemTitle: "普通建页", itemUrl: "" },
+      { itemTitle: "建页操作", itemUrl: "" }
+    ]
+  },
+  {
+    image: themeConfig,
+    title: "主题配置",
+    item: [
+      { itemTitle: "主题面板", itemUrl: "" },
+      { itemTitle: "设置公共值", itemUrl: "" },
+      { itemTitle: "设置组件库", itemUrl: "" }
+    ]
+  },
+  {
+    image: smartConfig,
+    title: "灵活配置",
+    item: [
+      { itemTitle: "创建页面层级", itemUrl: "" },
+      { itemTitle: "插件体系", itemUrl: "" },
+      { itemTitle: "块组件", itemUrl: "" }
+    ]
+  },
+  {
+    image: assistIllustrate,
+    title: "辅助说明",
+    item: [
+      { itemTitle: "设置", itemUrl: "" },
+      { itemTitle: "查看", itemUrl: "" },
+      { itemTitle: "导出", itemUrl: "" }
+    ]
+  },
+  {
+    image: advanceDev,
+    title: "高阶开发",
+    item: [
+      { itemTitle: "导出自定义组件", itemUrl: "" },
+      { itemTitle: "自定义模板", itemUrl: "" }
+    ]
+  }
+];
+
+const getFeatureReferenceCradItem = data => {
+  return (
+    data &&
+    data.map(item => {
+      const { itemTitle, itemUrl } = item;
+      return (
+        <Row>
+          <Col
+            span={0}
+            sm={{ span: 0 }}
+            md={{ span: 24 }}
+            xl={{ span: 24 }}
+            xxl={{ span: 24 }}
+          >
+            <FeatureReferenceCradItem>{itemTitle}</FeatureReferenceCradItem>
+          </Col>
+        </Row>
+      );
+    })
+  );
+};
+const getFeatureReferenceContent = data => {
+  return (
+    data &&
+    data.map(child => {
+      const { title, image, item } = child;
+      return (
+        <FeatureReferenceCradWrap>
+          <FeatureReferenceCradInner>
+            <ImageWrap bgImage={image} />
+            <FeatureReferenceCradTitle>{title}</FeatureReferenceCradTitle>
+            {getFeatureReferenceCradItem(item)}
+          </FeatureReferenceCradInner>
+        </FeatureReferenceCradWrap>
+      );
+    })
+  );
+};
+
+export default class TutorialCenter extends Component {
+  render() {
+    return (
+      <Fragment>
+        <BannerImg />
+        <Row>
+          <Col span={1} md={{ span: 1 }} xl={{ span: 2 }} xxl={{ span: 3 }}>
+            <Wrapper> </Wrapper>
+          </Col>
+          <Col span={22} md={{ span: 22 }} xl={{ span: 20 }} xxl={{ span: 18 }}>
+            <Header />
+            <SearchWrap>
+              <SearchHeader>Hi~请问有什么可以帮您？</SearchHeader>
+              <Theme config={searchTheme}>
+                <Input
+                  suffix={<Icon iconClass="lugia-icon-financial_search" />}
+                  placeholder="搜索答案"
+                />
+              </Theme>
+            </SearchWrap>
+            <Content>
+              <ContentTitleWrap>
+                <ContentTitle>基础教程</ContentTitle>
+              </ContentTitleWrap>
+              <Row
+                gutter={[
+                  { xs: 18, sm: 18, md: 18, xl: 18, xxl: 18, lg: 18 },
+                  { xs: 18, sm: 18, md: 18, xl: 18, xxl: 18, lg: 18 }
+                ]}
+              >
+                <Col span={8} sm={{ span: 24 }} xl={{ span: 8 }}>
+                  <VideoCardWrap>
+                    <VideoWrap>
+                      <iframe
+                        src="//player.bilibili.com/player.html?aid=67434572&cid=116925497&page=1"
+                        scrolling="no"
+                        border="0"
+                        frameBorder="no"
+                        framespacing="0"
+                        allowFullScreen="true"
+                      />
+                    </VideoWrap>
+                  </VideoCardWrap>
+                  <VideoTitle>新功能介绍</VideoTitle>
+                </Col>
+                <Col span={8} sm={{ span: 24 }} xl={{ span: 8 }}>
+                  <VideoCardWrap>
+                    <VideoWrap>
+                      <iframe
+                        src="//player.bilibili.com/player.html?aid=67434572&cid=116925497&page=1"
+                        scrolling="no"
+                        border="0"
+                        frameBorder="no"
+                        framespacing="0"
+                        allowFullScreen="true"
+                      />
+                    </VideoWrap>
+                  </VideoCardWrap>
+                  <VideoTitle>快速入手</VideoTitle>
+                </Col>
+                <Col span={8} sm={{ span: 24 }} xl={{ span: 8 }}>
+                  <VideoCardWrap>
+                    <VideoWrap>
+                      <iframe
+                        src="//player.bilibili.com/player.html?aid=67434572&cid=116925497&page=1"
+                        scrolling="no"
+                        border="0"
+                        frameBorder="no"
+                        framespacing="0"
+                        allowFullScreen="true"
+                      />
+                    </VideoWrap>
+                  </VideoCardWrap>
+                  <VideoTitle>常规面板</VideoTitle>
+                </Col>
+              </Row>
+
+              <ContentTitleWrap>
+                <ContentTitle>操作流程</ContentTitle>
+              </ContentTitleWrap>
+              <OperationalProcessWrap>
+                <OpreationProcess />
+              </OperationalProcessWrap>
+
+              <ContentTitleWrap>
+                <ContentTitle>功能导示</ContentTitle>
+              </ContentTitleWrap>
+              <FeatureReferenceWrap>
+                {getFeatureReferenceContent(featureReferenceData)}
+              </FeatureReferenceWrap>
+
+              <ContentTitleWrap>
+                <ContentTitle>无法解决您的问题?</ContentTitle>
+              </ContentTitleWrap>
+              <FeedbackWrap>
+                <Row
+                  gutter={[
+                    { xs: 20, sm: 20, md: 20, xl: 20, xxl: 20, lg: 20 },
+                    { xs: 20, sm: 20, md: 20, xl: 20, xxl: 20, lg: 20 }
+                  ]}
+                >
+                  <Col span={12} sm={{ span: 24 }} xl={{ span: 12 }}>
+                    <FeedbackCardWrap>
+                      <MailFeedbackCardWrapInner>
+                        <FeedbackImg bgImage={notebook}></FeedbackImg>
+                        <FeedbackContent>
+                          <FeedbackTitle
+                            onClick={() => linkToUrl("/tutorial/feedback")}
+                          >
+                            提交反馈
+                          </FeedbackTitle>
+                          <FeedbackDesc>
+                            欢迎将您需要的教程告诉我们，我们会定期根据大家的需求进行优化
+                          </FeedbackDesc>
+                        </FeedbackContent>
+                      </MailFeedbackCardWrapInner>
+                    </FeedbackCardWrap>
+                  </Col>
+                  <Col span={12} sm={{ span: 24 }} xl={{ span: 12 }}>
+                    <MailFeedbackCardWrap>
+                      <MailFeedbackCardWrapInner>
+                        <FeedbackImg bgImage={notebookMail}></FeedbackImg>
+                        <FeedbackContent>
+                          <FeedbackTitle href="mailto:Lugia@ysstech.com">
+                            Lugia@ysstech.com
+                          </FeedbackTitle>
+                          <FeedbackDesc>
+                            请在邮件中详细描述您的问题，我们将在工作日24小时内联系您
+                          </FeedbackDesc>
+                        </FeedbackContent>
+                      </MailFeedbackCardWrapInner>
+                    </MailFeedbackCardWrap>
+                  </Col>
+                </Row>
+              </FeedbackWrap>
+            </Content>
+          </Col>
+          <Col span={1} md={{ span: 1 }} xl={{ span: 2 }} xxl={{ span: 3 }}>
+            <Wrapper> </Wrapper>
+          </Col>
+        </Row>
+        <Footer />
+      </Fragment>
+    );
+  }
+}
