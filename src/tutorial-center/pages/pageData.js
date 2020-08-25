4e6ac6737081ed62adb602612478d160b5224201
Shine_Lee <wangyi_lixian@163.com>
2020-08-25 13:57:30 +0800

    Add: tutorial center change video url

--- src/tutorial-center/pages/pageData.js
+++ src/tutorial-center/pages/pageData.js
@@ -4,139 +4,139 @@ export const pageData = {
     title: "快速上手",
     desc: "控制组件的层级。成组。解锁等功能。",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "normal-built": {
     id: 2,
     title: "普通建页",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   group: {
     id: 3,
     title: "成组",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "component-tree": {
     id: 4,
     title: "组件树",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   align: {
     id: 5,
     title: "对齐",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "theme-quick-start": {
     id: 6,
     title: "快速上手",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "public-value-set": {
     id: 7,
     title: "设置公共值",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "component-set": {
     id: 8,
     title: "设置组件库",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "built-quick-start": {
     id: 9,
     title: "快速上手",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   drag: {
     id: 10,
     title: "拖拽/选择/替换组件",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   magnify: {
     id: 11,
     title: "放大/缩小/移动组件",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   copy: {
     id: 12,
     title: "复制/粘贴/移动组件",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "built-align": {
     id: 13,
     title: "对齐",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "reference-line": {
     id: 14,
     title: "参考线",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "component-level": {
     id: 15,
     title: "组件层级",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "component-group": {
     id: 16,
     title: "组件成组",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "change-position": {
     id: 17,
     title: "组件互换位置",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "page-list": {
     id: 18,
     title: "页面列表板",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "page-component": {
     id: 19,
     title: "页面组件树",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   },
   "layout-quick-start": {
     id: 20,
     title: "快速上手",
     desc: "待添加",
     videoSrc:
-      "https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4"
+      "http://www.w3school.com.cn/example/html5/mov_bbb.mp4"
   }
 };
