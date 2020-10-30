252bbfd64a27e3bb49fdc258b57bd700a61009a2
liyaqin <liyaqin@ysstech.com>
2020-10-31 01:15:58 +0800

    update：修改Anchor组件demo

--- src/widgets/anchor/TypeDemo.js
+++ src/widgets/anchor/TypeDemo.js
@@ -6,8 +6,8 @@ const { Link } = Anchor;
 export default class AnchorDemo extends React.Component {
     render(){
         return (
-            <div style={{marginLeft: '400px'}}>
-                <Anchor slideType="none">
+            <div style={{marginLeft: '400px'}}  id="anchor-2">
+                <Anchor slideType="none"  useHref={false}>
                   <Link title="基本用法" href="#anchor-0" />
                   <Link title="指定容器" href="#anchor-1" />
                   <Link title="固定状态改变的回调" href="#anchor-2" />
