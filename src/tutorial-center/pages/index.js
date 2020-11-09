7327265dc27c046188ff694a30fc1c772d49abaa
Shine_Lee <wangyi_lixian@163.com>
2020-11-09 14:20:45 +0800

    Fix: tutorial change

--- src/tutorial-center/pages/index.js
+++ src/tutorial-center/pages/index.js
@@ -25,11 +25,11 @@ export default () => {
     <OuterWrap>
       <Headers />
       <Row>
-        <Col span={6} sm={{span: 10}} md={{span: 9}} lg={{span: 8}} xl={{ span: 6 }}  xxl={{ span: 5 }}>
+        <Col span={5} sm={{span: 10}} md={{span: 9}} lg={{span: 8}} xl={{ span: 6 }}  xxl={{ span: 5 }}>
           <Asides />
           <Block />
         </Col>
-        <Col span={18} sm={{span: 14}} md={{span: 15}} lg={{span: 16}} xl={{ span: 18 }}  xxl={{ span: 19 }}>
+        <Col span={19} sm={{span: 14}} md={{span: 15}} lg={{span: 16}} xl={{ span: 18 }}  xxl={{ span: 19 }}>
           {rout}
         </Col>
       </Row>
